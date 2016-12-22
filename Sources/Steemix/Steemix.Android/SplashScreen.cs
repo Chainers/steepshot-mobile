09fc7a8d8f302ef2de43e7640b266d818dd5be87
KirillVolkov <KirillVolkov>
2016-12-22 23:17:07 +0300

    launcher_icons

---
+++ Sources/Steemix/Steemix.Android/SplashScreen.cs
@@ -0,0 +1,17 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace Steemix.Android
+{
+    [Activity(Label = "SteepShot", MainLauncher = true, Icon = "@mipmap/ic_launcher")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            SetContentView(Resource.Layout.Main);
+        }
+    }
+}
