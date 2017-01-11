e3f6f7843d5c36f6d628f3496c1917a95155d19f
korzun <A.Korzun@corp.profigroup.by>
2017-01-11 17:20:45 +0300

    step commit

--- Sources/Steemix/Steemix.Android/Views/SettingsActivity.cs
+++ Sources/Steemix/Steemix.Android/Views/SettingsActivity.cs
@@ -1,34 +1,36 @@
 using Android.App;
 using Android.OS;
 using Android.Widget;
+using Refractored.Controls;
 using Steemix.Droid.Activity;
+using Steemix.Droid.ViewModels;
 using Square.Picasso;
 
 namespace Steemix.Droid.Views
 {
     [Activity(NoHistory = true)]
-    public class SettingsActivity : BaseActivity<SignInViewModel>
+    public class SettingsActivity : BaseActivity<SettingsViewModel>
     {
         private ImageView Avatar;
 
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
+            var info = ViewModel.GetUserInfo();
+            info.Start();
 
             SetContentView(Resource.Layout.lyt_settings);
-            Avatar = FindViewById<ImageView>(Resource.Id.avatar);
+            Avatar = FindViewById<CircleImageView>(Resource.Id.avatar);
+            LoadAvatar();
+        }
 
-            //Picasso.With(context).Load(Posts[position].Body).Into(vh.Photo);
-
-            //Picasso.With(mContext)
-            //    .load(com.app.utility.Constants.BASE_URL + b.image)
-            //    .placeholder(R.drawable.profile)
-            //    .error(R.drawable.profile)
-            //    .transform(new RoundedTransformation(50, 4))
-            //    .resizeDimen(R.dimen.list_detail_image_size, R.dimen.list_detail_image_size)
-            //    .centerCrop()
-            //    .into(v.im_user);
-
+        private async void LoadAvatar()
+        {
+            var info = await ViewModel.GetUserInfo();
+            if (!string.IsNullOrEmpty(info.ImageUrl))
+            {
+                Picasso.With(this.ApplicationContext).Load(info.ImageUrl).Into(Avatar);
+            }
         }
     }
 }
