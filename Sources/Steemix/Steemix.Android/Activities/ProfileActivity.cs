207026f86ae0bc2ce74b2e38090e818ae99f125a
Kirill Volkov <k.volkov@pixelplex.by>
2017-01-20 22:32:11 +0300

    profile

--- Sources/Steemix/Steemix.Android/Activities/ProfileActivity.cs
+++ Sources/Steemix/Steemix.Android/Activities/ProfileActivity.cs
@@ -1,6 +1,13 @@
+﻿
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
 using Android.Content;
 using Android.OS;
+using Android.Runtime;
 using Android.Support.Design.Widget;
 using Android.Support.V7.Widget;
 using Android.Views;
@@ -8,40 +15,50 @@ using Android.Widget;
 using Com.Lilarcor.Cheeseknife;
 using Refractored.Controls;
 using Square.Picasso;
-using Steemix.Droid.Activities;
 
-namespace Steemix.Droid.Fragments
+namespace Steemix.Droid.Activities
 {
-	public class ProfileFragment : BaseFragment<ViewModels.ProfileViewModel>
+	[Activity(Label = "ProfileActivity",ScreenOrientation = Android.Content.PM.ScreenOrientation.Portrait)]
+	public class ProfileActivity : BaseActivity<ViewModels.UserProfileViewModel>
 	{
-		public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+		string ProfileId;
+		protected override void OnCreate(Bundle savedInstanceState)
 		{
-			var v = inflater.Inflate(Resource.Layout.lyt_fragment_profile, null);
-			Cheeseknife.Inject(this, v);
-			return v;
+			base.OnCreate(savedInstanceState);
+			SetContentView(Resource.Layout.lyt_fragment_profile);
+			Cheeseknife.Inject(this);
+			ProfileId = Intent.GetStringExtra("ID");
+			Settings.Visibility = ViewStates.Gone;
+			PostsList.SetLayoutManager(new GridLayoutManager(this, 3));
 		}
 
-		public override void OnViewCreated(View view, Bundle savedInstanceState)
+		protected override void OnResume()
 		{
-			base.OnViewCreated(view, savedInstanceState);
-			backButton.Visibility = ViewStates.Gone;
-			FollowButton.Visibility = ViewStates.Gone;
-			PostsList.SetLayoutManager(new GridLayoutManager(Context, 3));
+			base.OnResume();
 			LoadProfile();
 		}
 
-		[InjectOnClick(Resource.Id.btn_settings)]
-		public void OnSettingsClick(object sender, EventArgs e)
+
+		[InjectView(Resource.Id.btn_settings)]
+		ImageButton Settings;
+
+		[InjectOnClick(Resource.Id.follow_btn)]
+		public void OnFollowClick(object sender, EventArgs e)
 		{
-			var intent = new Intent(Context, typeof(SettingsActivity));
-			StartActivity(intent);
+			Toast.MakeText(this, "TODO", ToastLength.Short).Show();
+		}
+
+		[InjectOnClick(Resource.Id.btn_back)]
+		public void OnBackClick(object sender, EventArgs e)
+		{
+			OnBackPressed();
 		}
 
 
 		[InjectOnClick(Resource.Id.following_btn)]
 		public void OnFollowingClick(object sender, EventArgs e)
 		{
-			var intent = new Intent(Context, typeof(FollowersActivity));
+			var intent = new Intent(this, typeof(FollowersActivity));
 			intent.PutExtra("isFollowers", false);
 			StartActivity(intent);
 		}
@@ -49,7 +66,7 @@ namespace Steemix.Droid.Fragments
 		[InjectOnClick(Resource.Id.followers_btn)]
 		public void OnFollowersClick(object sender, EventArgs e)
 		{
-			var intent = new Intent(Context, typeof(FollowersActivity));
+			var intent = new Intent(this, typeof(FollowersActivity));
 			intent.PutExtra("isFollowers", true);
 			StartActivity(intent);
 		}
@@ -60,14 +77,14 @@ namespace Steemix.Droid.Fragments
 			if (PostsList.GetLayoutManager() is GridLayoutManager)
 			{
 				Switcher.SetImageResource(Resource.Drawable.ic_gray_grid);
-				PostsList.SetLayoutManager(new LinearLayoutManager(Context));
-				PostsList.SetAdapter(new Adapter.FeedAdapter(Context, ViewModel.UserPosts));
+				PostsList.SetLayoutManager(new LinearLayoutManager(this));
+				PostsList.SetAdapter(new Adapter.FeedAdapter(this, ViewModel.UserPosts));
 			}
 			else
-			{ 
+			{
 				Switcher.SetImageResource(Resource.Drawable.ic_gray_list);
-				PostsList.SetLayoutManager(new GridLayoutManager(Context, 3));
-				PostsList.SetAdapter(new Adapter.PostsGridAdapter(Context, ViewModel.UserPosts));
+				PostsList.SetLayoutManager(new GridLayoutManager(this, 3));
+				PostsList.SetAdapter(new Adapter.PostsGridAdapter(this, ViewModel.UserPosts));
 			}
 		}
 
@@ -121,24 +138,24 @@ namespace Steemix.Droid.Fragments
 
 		private async void LoadProfile()
 		{
-			var Profile = await ViewModel.GetUserInfo(UserPrincipal.CurrentUser.Login);
+			var Profile = await ViewModel.GetUserInfo(ProfileId);
+
+			if (UserPrincipal.CurrentUser.Login.Equals(Profile.Username))
+			{ 
+				FollowButton.Visibility = ViewStates.Gone;
+			}
+
 			ProfileName.Text = Profile.Username;
 			JoinedText.Text = Profile.LastAccountUpdate.ToString();
-			Picasso.With(this.Context).Load(Profile.ProfileImage).Resize(ProfileImage.Width, ProfileImage.Width).Into(ProfileImage);
-			CostButton.Text = (string.Format(GetString(Resource.String.cost_param_on_balance),Profile.EstimatedBalance));
+			Picasso.With(this).Load(Profile.ProfileImage).Into(ProfileImage);
+			CostButton.Text = (string.Format(GetString(Resource.String.cost_param_on_balance), Profile.EstimatedBalance));
 			PhotosCount.Text = Profile.PostCount.ToString();
 			FollowingCount.Text = Profile.FollowingCount.ToString();
 			FollowersCount.Text = Profile.FollowersCount.ToString();
 			spinner.Visibility = ViewStates.Gone;
 			Content.Visibility = ViewStates.Visible;
-			var Posts = await ViewModel.GetUserPosts();
-			PostsList.SetAdapter(new Adapter.PostsGridAdapter(Context, ViewModel.UserPosts));
-		}
-
-		public override void OnDestroyView()
-		{
-			base.OnDestroyView();
-			Cheeseknife.Reset(this);
+			await ViewModel.GetUserPosts();
+			PostsList.SetAdapter(new Adapter.PostsGridAdapter(this, ViewModel.UserPosts));
 		}
 	}
 }
