c7cf1ab8859982aca63d8c39650c6b7c72286b71
Nikita Rakitin <nnn93@mail.ru>
2017-05-17 09:50:21 +0300

    iOS photo upload fix

--- Sources/Steemix/Steepshot.iOS/ViewControllers/DescriptionViewController.cs
+++ Sources/Steemix/Steepshot.iOS/ViewControllers/DescriptionViewController.cs
@@ -1,94 +1,99 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Runtime.InteropServices;
+using System.Threading;
 using System.Threading.Tasks;
-using CoreGraphics;
 using Foundation;
-using Sweetshot.Library.Models.Common;
 using Sweetshot.Library.Models.Requests;
-using Sweetshot.Library.Models.Responses;
 using UIKit;
 
 namespace Steepshot.iOS
 {
-    public partial class DescriptionViewController : BaseViewController
-    {
-        protected DescriptionViewController(IntPtr handle) : base(handle)
-        {
-            // Note: this .ctor should not contain any initialization logic.
-        }
+	public partial class DescriptionViewController : BaseViewController
+	{
+		protected DescriptionViewController(IntPtr handle) : base(handle)
+		{
+			// Note: this .ctor should not contain any initialization logic.
+		}
 
-        public UIImage ImageAsset;
-       
-        private TagsCollectionViewSource collectionviewSource;
+		public UIImage ImageAsset;
 
-        public override void ViewDidLoad()
-        {
-            base.ViewDidLoad();
-            photoView.Image = ImageAsset;
-            postPhotoButton.TouchDown += (sender, e) => PostPhoto();
+		private TagsCollectionViewSource collectionviewSource;
 
-            //Collection view initialization
-            tagsCollectionView.RegisterClassForCell(typeof(TagCollectionViewCell), nameof(TagCollectionViewCell));
-            tagsCollectionView.RegisterNibForCell(UINib.FromName(nameof(TagCollectionViewCell), NSBundle.MainBundle), nameof(TagCollectionViewCell));
-            // research flow layout
-            /*tagsCollectionView.SetCollectionViewLayout(new UICollectionViewFlowLayout()
+		public override void ViewDidLoad()
+		{
+			base.ViewDidLoad();
+			photoView.Image = ImageAsset;
+			postPhotoButton.TouchDown += (sender, e) => PostPhoto();
+
+			//Collection view initialization
+			tagsCollectionView.RegisterClassForCell(typeof(TagCollectionViewCell), nameof(TagCollectionViewCell));
+			tagsCollectionView.RegisterNibForCell(UINib.FromName(nameof(TagCollectionViewCell), NSBundle.MainBundle), nameof(TagCollectionViewCell));
+			// research flow layout
+			/*tagsCollectionView.SetCollectionViewLayout(new UICollectionViewFlowLayout()
             {
                 EstimatedItemSize = new CGSize(100, 50),
                 
             }, false);*/
-            collectionviewSource = new TagsCollectionViewSource((sender, e) =>
-            {
-                var myViewController = Storyboard.InstantiateViewController(nameof(PostTagsViewController)) as PostTagsViewController;
-                this.NavigationController.PushViewController(myViewController, true);
-            });
-            collectionviewSource.tagsCollection = new List<string>() { "" }; //UserContext.Instanse.TagsList;
-            tagsCollectionView.Source = collectionviewSource;
+			collectionviewSource = new TagsCollectionViewSource((sender, e) =>
+			{
+				var myViewController = Storyboard.InstantiateViewController(nameof(PostTagsViewController)) as PostTagsViewController;
+				this.NavigationController.PushViewController(myViewController, true);
+			});
+			collectionviewSource.tagsCollection = new List<string>() { "" }; //UserContext.Instanse.TagsList;
+			tagsCollectionView.Source = collectionviewSource;
 
 			UITapGestureRecognizer tap = new UITapGestureRecognizer(() =>
 				{
 					descriptionTextField.ResignFirstResponder();
 				});
 			this.View.AddGestureRecognizer(tap);
+			descriptionTextField.Layer.BorderWidth = 1;
+			descriptionTextField.Layer.BorderColor = UIColor.Black.CGColor;
+		}
 
-        }
+		public override void ViewDidAppear(bool animated)
+		{
+			base.ViewDidAppear(animated);
 
-        public override void ViewDidAppear(bool animated)
-        {
-            base.ViewDidAppear(animated);
+			collectionviewSource.tagsCollection.Clear();
+			collectionviewSource.tagsCollection.Add("");
+			collectionviewSource.tagsCollection.AddRange(UserContext.Instanse.TagsList);
+			tagsCollectionView.ReloadData();
+		}
 
-            collectionviewSource.tagsCollection.Clear();
-            collectionviewSource.tagsCollection.Add("");
-            collectionviewSource.tagsCollection.AddRange(UserContext.Instanse.TagsList);
-            tagsCollectionView.ReloadData();
-        }
-
-        public override void ViewDidDisappear(bool animated)
-        {
+		public override void ViewDidDisappear(bool animated)
+		{
 			if (IsMovingFromParentViewController)
 				UserContext.Instanse.TagsList.Clear();
-            base.ViewDidDisappear(animated);
-        }
+			base.ViewDidDisappear(animated);
+		}
 
-        private async Task PostPhoto()
-        {
-            loadingView.Hidden = false;
-			NSData imageData;
-            try
-            {
-				imageData = photoView.Image.AsPNG();
-                var myByteArray = new Byte[imageData.Length];
-                Marshal.Copy(imageData.Bytes, myByteArray, 0, Convert.ToInt32(imageData.Length));
-                var request = new UploadImageRequest(UserContext.Instanse.Token, descriptionTextField.Text, myByteArray, UserContext.Instanse.TagsList.ToArray());
-                var imageUploadResponse = await Api.Upload(request);
-                if (imageUploadResponse.Success)
-                {
-                    UserContext.Instanse.TagsList.Clear();
+		private async Task PostPhoto()
+		{
+			loadingView.Hidden = false;
+
+			try
+			{
+				byte[] photoByteArray;
+				using (NSData imageData = photoView.Image.AsJPEG(0.8f))
+				{
+					photoByteArray = new Byte[imageData.Length];
+					Marshal.Copy(imageData.Bytes, photoByteArray, 0, Convert.ToInt32(imageData.Length));
+				}
+
+				var request = new UploadImageRequest(UserContext.Instanse.Token, descriptionTextField.Text, photoByteArray, UserContext.Instanse.TagsList.ToArray());
+				var imageUploadResponse = await Api.Upload(request);
+
+				if (imageUploadResponse.Success)
+				{
+					UserContext.Instanse.TagsList.Clear();
 					UserContext.Instanse.ShouldProfileUpdate = true;
-                    this.NavigationController.PopViewController(true);
-                }
-                else
-                {
+					this.NavigationController.PopViewController(true);
+				}
+				else
+				{
 					//logging
 					UIAlertView alert = new UIAlertView()
 					{
@@ -97,19 +102,15 @@ namespace Steepshot.iOS
 					alert.AddButton("OK");
 					alert.Show();
 				}
-                
-            }
-            catch (Exception ex)
-            {
-                //show alert + logging
-            }
-            finally
-            {
-				imageData = null;
-				imageData.Dispose();
-                loadingView.Hidden = true;
-            }
-        }
-    }
+			}
+			catch (Exception ex)
+			{
+				//show alert + logging
+			}
+			finally
+			{
+				loadingView.Hidden = true;
+			}
+		}
+	}
 }
-
