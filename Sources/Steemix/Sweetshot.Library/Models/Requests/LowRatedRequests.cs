57c84e892ca46352eb3ac9482cee65e6ef7f2e7d
Nikita Rakitin <nnn93@mail.ru>
2017-06-08 10:11:40 +0300

    Low rated trigger added

---
+++ Sources/Steemix/Sweetshot.Library/Models/Requests/LowRatedRequests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Xml.Xsl;
+using Newtonsoft.Json;
+
+namespace Sweetshot.Library.Models.Requests
+{
+    public class IsLowRatedRequest : SessionIdField
+    {
+        public IsLowRatedRequest(string sessionId)
+        {
+            if (string.IsNullOrWhiteSpace(sessionId)) throw new ArgumentNullException(nameof(sessionId));
+            base.SessionId = sessionId;
+        }
+    }
+
+    public class SetLowRatedRequest : IsLowRatedRequest
+    {
+        public SetLowRatedRequest(string sessionId, bool showLowRated) : base(sessionId)
+        {
+            ShowLowRated = showLowRated;
+        }
+
+        [JsonProperty(PropertyName = "show_low_rated")]
+        public bool ShowLowRated { get; private set; }
+    }
+}
