5b3053eb28510fe1e10b0662dd1b3d7ec5036339
Nikita Rakitin <nnn93@mail.ru>
2017-06-09 22:01:46 +0300

    low rated + nsfw

---
+++ Sources/Steemix/Sweetshot.Library/Models/Requests/NsfwRequest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace Sweetshot.Library.Models.Requests
+{
+	public class IsNsfwRequest : SessionIdField
+	{
+		public IsNsfwRequest(string sessionId)
+		{
+			if (string.IsNullOrWhiteSpace(sessionId)) throw new ArgumentNullException(nameof(sessionId));
+			base.SessionId = sessionId;
+		}
+	}
+
+	public class SetNsfwRequest : IsNsfwRequest
+	{
+		public SetNsfwRequest(string sessionId, bool showNsfw) : base(sessionId)
+		{
+			ShowNsfw = showNsfw;
+		}
+
+		[JsonProperty(PropertyName = "show_nsfw")]
+		public bool ShowNsfw { get; private set; }
+	}
+}
