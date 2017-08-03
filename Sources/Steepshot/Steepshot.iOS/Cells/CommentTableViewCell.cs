50d11c97335ee71cae0139980ef6c4fcb87c0e82
KorzunAV <KorzunAV@gmail.com>
2017-08-03 22:57:05 +0300

    fix dataformat fix money kof (static CurencyConvertationDic added)

--- Sources/Steepshot/Steepshot.iOS/Cells/CommentTableViewCell.cs
+++ Sources/Steepshot/Steepshot.iOS/Cells/CommentTableViewCell.cs
@@ -47,7 +47,7 @@ namespace Steepshot.iOS.Cells
             bodyLabel.Text = _currentPost.Body;
             loginLabel.Text = _currentPost.Author;
             likeLabel.Text = _currentPost.NetVotes.ToString();
-            costLabel.Text = _currentPost.TotalPayoutReward.ToCurrencyString(BaseViewController.Currency);
+            costLabel.Text = BaseViewController.ToFormatedCurrencyString(_currentPost.TotalPayoutReward);
             likeButton.Selected = _currentPost.Vote;
             likeButton.Enabled = true;
 
