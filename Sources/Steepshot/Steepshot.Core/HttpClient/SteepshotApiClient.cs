2ff1e30d9b454582d3cc91e91883e71715ca4159
AnCh <anton_chug@yahoo.com>
2017-08-04 14:59:18 +0300

    Implemented cancelation

--- Sources/Steepshot/Steepshot.Core/HttpClient/SteepshotApiClient.cs
+++ Sources/Steepshot/Steepshot.Core/HttpClient/SteepshotApiClient.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Text.RegularExpressions;
+using System.Threading;
 using System.Threading.Tasks;
 using RestSharp.Portable;
 using Steepshot.Core.Extensions;
@@ -24,12 +25,12 @@ namespace Steepshot.Core.HttpClient
             _jsonConverter = new JsonNetConverter();
         }
 
-        public async Task<OperationResult<LoginResponse>> LoginWithPostingKey(LoginWithPostingKeyRequest request)
+        public async Task<OperationResult<LoginResponse>> LoginWithPostingKey(LoginWithPostingKeyRequest request, CancellationTokenSource cts = null)
         {
-            return await Authenticate("login-with-posting", request);
+            return await Authenticate("login-with-posting", request, cts);
         }
 
-        private async Task<OperationResult<LoginResponse>> Authenticate(string endpoint, ILoginRequest request)
+        private async Task<OperationResult<LoginResponse>> Authenticate(string endpoint, ILoginRequest request, CancellationTokenSource cts)
         {
             var parameters = new List<RequestParameter>
             {
@@ -41,7 +42,7 @@ namespace Steepshot.Core.HttpClient
                 }
             };
 
-            var response = await _gateway.Post(endpoint, parameters);
+            var response = await _gateway.Post(endpoint, parameters, cts);
 
             var errorResult = CheckErrors(response);
             var result = CreateResult<LoginResponse>(response.Content, errorResult);
@@ -64,65 +65,65 @@ namespace Steepshot.Core.HttpClient
             return result;
         }
 
-        public async Task<OperationResult<UserPostResponse>> GetUserPosts(UserPostsRequest request)
+        public async Task<OperationResult<UserPostResponse>> GetUserPosts(UserPostsRequest request, CancellationTokenSource cts = null)
         {
             var parameters = CreateSessionParameter(request.SessionId);
             var parameters2 = CreateOffsetLimitParameters(request.Offset, request.Limit);
             parameters2.AddRange(parameters);
 
-            var response = await _gateway.Get($"user/{request.Username}/posts", parameters2);
+            var response = await _gateway.Get($"user/{request.Username}/posts", parameters2, cts);
             var errorResult = CheckErrors(response);
             return CreateResult<UserPostResponse>(response.Content, errorResult);
         }
 
-        public async Task<OperationResult<UserPostResponse>> GetUserRecentPosts(UserRecentPostsRequest request)
+        public async Task<OperationResult<UserPostResponse>> GetUserRecentPosts(UserRecentPostsRequest request, CancellationTokenSource cts = null)
         {
             var parameters = CreateSessionParameter(request.SessionId);
             var parameters2 = CreateOffsetLimitParameters(request.Offset, request.Limit);
             parameters2.AddRange(parameters);
 
-            var response = await _gateway.Get("recent", parameters2);
+            var response = await _gateway.Get("recent", parameters2, cts);
             var errorResult = CheckErrors(response);
             return CreateResult<UserPostResponse>(response.Content, errorResult);
         }
 
-        public async Task<OperationResult<UserPostResponse>> GetPosts(PostsRequest request)
+        public async Task<OperationResult<UserPostResponse>> GetPosts(PostsRequest request, CancellationTokenSource cts = null)
         {
             var parameters = CreateSessionParameter(request.SessionId);
             var parameters2 = CreateOffsetLimitParameters(request.Offset, request.Limit);
             parameters2.AddRange(parameters);
 
             var endpoint = $"posts/{request.Type.ToString().ToLowerInvariant()}";
-            var response = await _gateway.Get(endpoint, parameters2);
+            var response = await _gateway.Get(endpoint, parameters2, cts);
             var errorResult = CheckErrors(response);
             return CreateResult<UserPostResponse>(response.Content, errorResult);
         }
 
-        public async Task<OperationResult<UserPostResponse>> GetPostsByCategory(PostsByCategoryRequest request)
+        public async Task<OperationResult<UserPostResponse>> GetPostsByCategory(PostsByCategoryRequest request, CancellationTokenSource cts = null)
         {
             var parameters = CreateSessionParameter(request.SessionId);
             var parameters2 = CreateOffsetLimitParameters(request.Offset, request.Limit);
             parameters2.AddRange(parameters);
 
             var endpoint = $"posts/{request.Category}/{request.Type.ToString().ToLowerInvariant()}";
-            var response = await _gateway.Get(endpoint, parameters2);
+            var response = await _gateway.Get(endpoint, parameters2, cts);
             var errorResult = CheckErrors(response);
             return CreateResult<UserPostResponse>(response.Content, errorResult);
         }
 
-        public async Task<OperationResult<SearchResponse<VotersResult>>> GetPostVoters(InfoRequest request)
+        public async Task<OperationResult<SearchResponse<VotersResult>>> GetPostVoters(InfoRequest request, CancellationTokenSource cts = null)
         {
             var parameters = CreateSessionParameter(request.SessionId);
             //var parameters2 = CreateOffsetLimitParameters(request.Offset, request.Limit);
             //parameters2.AddRange(parameters);
 
             var endpoint = $"post/{request.Url}/voters";
-            var response = await _gateway.Get(endpoint, parameters);
+            var response = await _gateway.Get(endpoint, parameters, cts);
             var errorResult = CheckErrors(response);
             return CreateResult<SearchResponse<VotersResult>>(response.Content, errorResult);
         }
 
-        public async Task<OperationResult<VoteResponse>> Vote(VoteRequest request)
+        public async Task<OperationResult<VoteResponse>> Vote(VoteRequest request, CancellationTokenSource cts = null)
         {
             var parameters = CreateSessionParameter(request.SessionId);
             parameters.Add(new RequestParameter
@@ -133,30 +134,30 @@ namespace Steepshot.Core.HttpClient
             });
 
             var endpoint = $"post/{request.Identifier}/{request.Type.GetDescription()}";
-            var response = await _gateway.Post(endpoint, parameters);
+            var response = await _gateway.Post(endpoint, parameters, cts);
             var errorResult = CheckErrors(response);
             return CreateResult<VoteResponse>(response.Content, errorResult);
         }
 
-        public async Task<OperationResult<FollowResponse>> Follow(FollowRequest request)
+        public async Task<OperationResult<FollowResponse>> Follow(FollowRequest request, CancellationTokenSource cts = null)
         {
             var parameters = CreateSessionParameter(request.SessionId);
 
             var endpoint = $"user/{request.Username}/{request.Type.ToString().ToLowerInvariant()}";
-            var response = await _gateway.Post(endpoint, parameters);
+            var response = await _gateway.Post(endpoint, parameters, cts);
             var errorResult = CheckErrors(response);
             return CreateResult<FollowResponse>(response.Content, errorResult);
         }
 
-        public async Task<OperationResult<GetCommentResponse>> GetComments(InfoRequest request)
+        public async Task<OperationResult<GetCommentResponse>> GetComments(InfoRequest request, CancellationTokenSource cts = null)
         {
             var parameters = CreateSessionParameter(request.SessionId);
-            var response = await _gateway.Get($"post/{request.Url}/comments", parameters);
+            var response = await _gateway.Get($"post/{request.Url}/comments", parameters, cts);
             var errorResult = CheckErrors(response);
             return CreateResult<GetCommentResponse>(response.Content, errorResult);
         }
 
-        public async Task<OperationResult<CreateCommentResponse>> CreateComment(CreateCommentRequest request)
+        public async Task<OperationResult<CreateCommentResponse>> CreateComment(CreateCommentRequest request, CancellationTokenSource cts = null)
         {
             var parameters = CreateSessionParameter(request.SessionId);
             parameters.Add(new RequestParameter
@@ -166,112 +167,112 @@ namespace Steepshot.Core.HttpClient
                 Type = ParameterType.RequestBody
             });
 
-            var response = await _gateway.Post($"post/{request.Url}/comment", parameters);
+            var response = await _gateway.Post($"post/{request.Url}/comment", parameters, cts);
             var errorResult = CheckErrors(response);
             return CreateResult<CreateCommentResponse>(response.Content, errorResult);
         }
 
-        public async Task<OperationResult<ImageUploadResponse>> Upload(UploadImageRequest request)
+        public async Task<OperationResult<ImageUploadResponse>> Upload(UploadImageRequest request, CancellationTokenSource cts = null)
         {
             var parameters = CreateSessionParameter(request.SessionId);
 
-            var response = await _gateway.Upload("post", request.Title, request.Photo, parameters, request.Tags);
+            var response = await _gateway.Upload("post", request.Title, request.Photo, parameters, request.Tags, cts);
             var errorResult = CheckErrors(response);
             return CreateResult<ImageUploadResponse>(response.Content, errorResult);
         }
 
-        public async Task<OperationResult<SearchResponse<SearchResult>>> GetCategories(SearchRequest request)
+        public async Task<OperationResult<SearchResponse<SearchResult>>> GetCategories(SearchRequest request, CancellationTokenSource cts = null)
         {
             var parameters = CreateSessionParameter(request.SessionId);
             var parameters2 = CreateOffsetLimitParameters(request.Offset, request.Limit);
             parameters2.AddRange(parameters);
 
-            var response = await _gateway.Get("categories/top", parameters2);
+            var response = await _gateway.Get("categories/top", parameters2, cts);
             var errorResult = CheckErrors(response);
             return CreateResult<SearchResponse<SearchResult>>(response.Content, errorResult);
         }
 
-        public async Task<OperationResult<SearchResponse<SearchResult>>> SearchCategories(SearchWithQueryRequest request)
+        public async Task<OperationResult<SearchResponse<SearchResult>>> SearchCategories(SearchWithQueryRequest request, CancellationTokenSource cts = null)
         {
             var parameters = CreateSessionParameter(request.SessionId);
             var parameters2 = CreateOffsetLimitParameters(request.Offset, request.Limit);
             parameters2.AddRange(parameters);
             parameters2.Add(new RequestParameter {Key = "query", Value = request.Query, Type = ParameterType.QueryString});
 
-            var response = await _gateway.Get("categories/search", parameters2);
+            var response = await _gateway.Get("categories/search", parameters2, cts);
             var errorResult = CheckErrors(response);
             return CreateResult<SearchResponse<SearchResult>>(response.Content, errorResult);
         }
 
-        public async Task<OperationResult<LogoutResponse>> Logout(LogoutRequest request)
+        public async Task<OperationResult<LogoutResponse>> Logout(LogoutRequest request, CancellationTokenSource cts = null)
         {
             var parameters = CreateSessionParameter(request.SessionId);
 
-            var response = await _gateway.Post("logout", parameters);
+            var response = await _gateway.Post("logout", parameters, cts);
             var errorResult = CheckErrors(response);
             return CreateResult<LogoutResponse>(response.Content, errorResult);
         }
 
-        public async Task<OperationResult<UserProfileResponse>> GetUserProfile(UserProfileRequest request)
+        public async Task<OperationResult<UserProfileResponse>> GetUserProfile(UserProfileRequest request, CancellationTokenSource cts = null)
         {
             var parameters = CreateSessionParameter(request.SessionId);
 
-            var response = await _gateway.Get($"user/{request.Username}/info", parameters);
+            var response = await _gateway.Get($"user/{request.Username}/info", parameters, cts);
             var errorResult = CheckErrors(response);
             return CreateResult<UserProfileResponse>(response.Content, errorResult);
         }
 
-        public async Task<OperationResult<UserFriendsResponse>> GetUserFriends(UserFriendsRequest request)
+        public async Task<OperationResult<UserFriendsResponse>> GetUserFriends(UserFriendsRequest request, CancellationTokenSource cts = null)
         {
             var parameters = CreateSessionParameter(request.SessionId);
             var parameters2 = CreateOffsetLimitParameters(request.Offset, request.Limit);
             parameters2.AddRange(parameters);
 
             var endpoint = $"user/{request.Username}/{request.Type.ToString().ToLowerInvariant()}";
-            var response = await _gateway.Get(endpoint, parameters2);
+            var response = await _gateway.Get(endpoint, parameters2, cts);
             var errorResult = CheckErrors(response);
             return CreateResult<UserFriendsResponse>(response.Content, errorResult);
         }
 
-        public async Task<OperationResult<TermOfServiceResponse>> TermsOfService()
+        public async Task<OperationResult<TermOfServiceResponse>> TermsOfService(CancellationTokenSource cts = null)
         {
             const string endpoint = "tos";
-            var response = await _gateway.Get(endpoint, new List<RequestParameter>());
+            var response = await _gateway.Get(endpoint, new List<RequestParameter>(), cts);
             var errorResult = CheckErrors(response);
             return CreateResult<TermOfServiceResponse>(response.Content, errorResult);
         }
 
-        public async Task<OperationResult<Post>> GetPostInfo(InfoRequest request)
+        public async Task<OperationResult<Post>> GetPostInfo(InfoRequest request, CancellationTokenSource cts = null)
         {
             var parameters = CreateSessionParameter(request.SessionId);
 
             var endpoint = $"post/{request.Url}/info";
-            var response = await _gateway.Get(endpoint, parameters);
+            var response = await _gateway.Get(endpoint, parameters, cts);
             var errorResult = CheckErrors(response);
             return CreateResult<Post>(response.Content, errorResult);
         }
 
-        public async Task<OperationResult<SearchResponse<UserSearchResult>>> SearchUser(SearchWithQueryRequest request)
+        public async Task<OperationResult<SearchResponse<UserSearchResult>>> SearchUser(SearchWithQueryRequest request, CancellationTokenSource cts = null)
         {
             var parameters = CreateSessionParameter(request.SessionId);
             var parameters2 = CreateOffsetLimitParameters(request.Offset, request.Limit);
             parameters2.AddRange(parameters);
             parameters2.Add(new RequestParameter {Key = "query", Value = request.Query, Type = ParameterType.QueryString});
 
-            var response = await _gateway.Get("user/search", parameters2);
+            var response = await _gateway.Get("user/search", parameters2, cts);
             var errorResult = CheckErrors(response);
             return CreateResult<SearchResponse<UserSearchResult>>(response.Content, errorResult);
         }
 
-        public async Task<OperationResult<UserExistsResponse>> UserExistsCheck(UserExistsRequests request)
+        public async Task<OperationResult<UserExistsResponse>> UserExistsCheck(UserExistsRequests request, CancellationTokenSource cts = null)
         {
             var endpoint = $"user/{request.Username}/exists";
-            var response = await _gateway.Get(endpoint, new List<RequestParameter>());
+            var response = await _gateway.Get(endpoint, new List<RequestParameter>(), cts);
             var errorResult = CheckErrors(response);
             return CreateResult<UserExistsResponse>(response.Content, errorResult);
         }
 
-        public async Task<OperationResult<FlagResponse>> Flag(FlagRequest request)
+        public async Task<OperationResult<FlagResponse>> Flag(FlagRequest request, CancellationTokenSource cts = null)
         {
             var parameters = CreateSessionParameter(request.SessionId);
             parameters.Add(new RequestParameter
@@ -282,7 +283,7 @@ namespace Steepshot.Core.HttpClient
             });
 
             var endpoint = $"post/{request.Identifier}/{request.Type.GetDescription()}";
-            var response = await _gateway.Post(endpoint, parameters);
+            var response = await _gateway.Post(endpoint, parameters, cts);
             var errorResult = CheckErrors(response);
             return CreateResult<FlagResponse>(response.Content, errorResult);
         }
