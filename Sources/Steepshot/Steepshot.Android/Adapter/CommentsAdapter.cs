078ac61193c72155e3d1630cf545926810738386
KorzunAV <KorzunAV@gmail.com>
2017-08-07 17:57:33 +0300

    fix redirect to user profile

--- Sources/Steepshot/Steepshot.Android/Adapter/CommentsAdapter.cs
+++ Sources/Steepshot/Steepshot.Android/Adapter/CommentsAdapter.cs
@@ -83,6 +83,7 @@ namespace Steepshot.Adapter
                 Like.Click += Like_Click;
                 Avatar.Click += (sender, e) => userAction?.Invoke(AdapterPosition);
                 Author.Click += (sender, e) => userAction?.Invoke(AdapterPosition);
+                Cost.Click += (sender, e) => userAction?.Invoke(AdapterPosition);
             }
 
             void Like_Click(object sender, EventArgs e)
