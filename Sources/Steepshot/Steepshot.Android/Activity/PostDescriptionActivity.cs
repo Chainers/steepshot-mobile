93d7ebcaeb0b2df86d8cb401dac5d1c3d9faab5c
KorzunAV <KorzunAV@gmail.com>
2017-08-15 20:32:30 +0300

    partial merge

--- Sources/Steepshot/Steepshot.Android/Activity/PostDescriptionActivity.cs
+++ Sources/Steepshot/Steepshot.Android/Activity/PostDescriptionActivity.cs
@@ -1,12 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Android.App;
 using Android.Content;
 using Android.Graphics;
-using Android.Graphics.Drawables;
 using Android.Media;
 using Android.OS;
 using Android.Views;
