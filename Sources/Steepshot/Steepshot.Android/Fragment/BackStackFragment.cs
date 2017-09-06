41e98585e234e282825ac98c019ed4996d9150b9
Nikita Rakitin <nnn93@mail.ru>
2017-09-06 15:34:48 +0300

    Feed fix

--- Sources/Steepshot/Steepshot.Android/Fragment/BackStackFragment.cs
+++ Sources/Steepshot/Steepshot.Android/Fragment/BackStackFragment.cs
@@ -2,39 +2,39 @@
 
 namespace Steepshot.Fragment
 {
-	public class BackStackFragment : Android.Support.V4.App.Fragment
-	{
-		public bool HandleBackPressed(FragmentManager fm)
-		{
-			if (fm?.Fragments != null)
-			{
-				foreach (var frag in fm.Fragments)
-				{
-					if (frag != null && frag.IsVisible && frag is BackStackFragment)
-					{
-						if (((BackStackFragment)frag).OnBackPressed())
-						{
-							return true;
-						}
-					}
-				}
-			}
-			return false;
-		}
+    public class BackStackFragment : Android.Support.V4.App.Fragment
+    {
+        public bool HandleBackPressed(FragmentManager fm)
+        {
+            if (fm?.Fragments != null)
+            {
+                foreach (var frag in fm.Fragments)
+                {
+                    if (frag != null && frag.IsVisible && frag is BackStackFragment)
+                    {
+                        if (((BackStackFragment)frag).OnBackPressed())
+                        {
+                            return true;
+                        }
+                    }
+                }
+            }
+            return false;
+        }
 
-		protected bool OnBackPressed()
-		{
-			var fm = ChildFragmentManager;
-			if (HandleBackPressed(fm))
-			{
-				return true;
-			}
-			else if (UserVisibleHint && fm.BackStackEntryCount > 0)
-			{
-				fm.PopBackStack();
-				return true;
-			}
-			return false;
-		}
-	}
+        protected bool OnBackPressed()
+        {
+            var fm = ChildFragmentManager;
+            if (HandleBackPressed(fm))
+            {
+                return true;
+            }
+            else if (UserVisibleHint && fm.BackStackEntryCount > 0)
+            {
+                fm.PopBackStack();
+                return true;
+            }
+            return false;
+        }
+    }
 }
