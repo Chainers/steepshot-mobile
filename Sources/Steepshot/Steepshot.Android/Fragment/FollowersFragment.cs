408c22055b48a0bffd98f6273f39ac6a07fd254e
Nikita Rakitin <nnn93@mail.ru>
2017-09-07 11:31:45 +0300

    android task canceled exception fix + reporter refactoring

--- Sources/Steepshot/Steepshot.Android/Fragment/FollowersFragment.cs
+++ Sources/Steepshot/Steepshot.Android/Fragment/FollowersFragment.cs
@@ -77,7 +77,7 @@ namespace Steepshot.Fragment
             }
             catch (Exception ex)
             {
-                Reporter.SendCrash(ex, BasePresenter.User.Login, BasePresenter.AppVersion);
+                Reporter.SendCrash(ex);
             }
         }
 
