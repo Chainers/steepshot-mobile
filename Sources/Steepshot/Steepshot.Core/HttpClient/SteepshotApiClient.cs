1ed45457d76f22572382f04b845c725ab71d1722
KorzunAV <KorzunAV@gmail.com>
2017-09-15 15:43:27 +0300

    fix ParameterType

--- Sources/Steepshot/Steepshot.Core/HttpClient/SteepshotApiClient.cs
+++ Sources/Steepshot/Steepshot.Core/HttpClient/SteepshotApiClient.cs
@@ -24,7 +24,7 @@ namespace Steepshot.Core.HttpClient
                 {
                     Key = "application/json",
                     Value = request,
-                    Type = ParameterType.GetOrPost
+                    Type = ParameterType.RequestBody
                 }
             };
 
@@ -58,7 +58,7 @@ namespace Steepshot.Core.HttpClient
             {
                 Key = "application/json",
                 Value = request,
-                Type = ParameterType.GetOrPost
+                Type = ParameterType.RequestBody
             });
 
             var response = await Gateway.Post($"post/{request.Identifier}/{request.Type.GetDescription()}", parameters, cts);
@@ -82,7 +82,7 @@ namespace Steepshot.Core.HttpClient
             {
                 Key = "application/json",
                 Value = request,
-                Type = ParameterType.GetOrPost
+                Type = ParameterType.RequestBody
             });
 
             var response = await Gateway.Post($"post/{request.Url}/comment", parameters, cts);
