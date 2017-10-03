95053c160841922ff95a16b6718b61c628a12ce9
KorzunAV <KorzunAV@gmail.com>
2017-10-03 15:20:30 +0300

    added stub Api and ReportService setted for debug mode

---
+++ Sources/Steepshot/Steepshot.Core/Utils/StubReporterService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Steepshot.Core.Services;
+
+namespace Steepshot.Core.Utils
+{
+    public class StubReporterService : IReporterService
+    {
+        public void SendCrash(Exception ex)
+        {
+            Console.WriteLine($"{ex.Message}{Environment.NewLine}{ex.StackTrace}");
+        }
+    }
+}
