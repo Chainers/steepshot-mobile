78a7cef62d0a5486fca2525e5219f2467de68e89
Nikita Rakitin <nnn93@mail.ru>
2017-10-05 12:26:30 +0300

    Properly working camera

---
+++ Sources/Steepshot/Steepshot.Android/Utils/CameraOrientationEventListener.cs
@@ -0,0 +1,31 @@
+﻿using Android.Content;
+using Android.Hardware;
+using Android.Runtime;
+using Android.Views;
+
+namespace Steepshot.Utils
+{
+    public class CameraOrientationEventListener : OrientationEventListener
+    {
+        public int Orientation;
+        //array for normal orientation handling(not camera)
+        //private static readonly int[] lookup = new [] { 0, 0, 0, 90, 90, 90, 90, 90, 90, 180, 180, 180, 180, 180, 180, 270, 270, 270, 270, 270, 270, 0, 0, 0 }; // 15-degree increments 
+        private static readonly int[] lookup = new[] { 90, 90, 90, 180, 180, 180, 180, 180, 180, 270, 270, 270, 270, 270, 270, 0, 0, 0, 0, 0, 0, 90, 90, 90 };
+
+        public CameraOrientationEventListener(Context context, [GeneratedEnum] SensorDelay rate) : base(context, rate)
+        {
+        }
+
+        public override void OnOrientationChanged(int orientation)
+        {
+            if (orientation != OrientationUnknown)
+            {
+                var newOrientation =  lookup[orientation / 15];
+                if (Orientation != newOrientation)
+                {
+                    Orientation = newOrientation;
+                }
+            }
+        }
+    }
+}
