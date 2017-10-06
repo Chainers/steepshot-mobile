a33252579edb7753ff6c8c178f6ed6336a466f36
Nikita Rakitin <nnn93@mail.ru>
2017-10-06 17:21:18 +0300

    new camera, but photo orientation problem on samsung

--- Sources/Steepshot/Steepshot.Android/Utils/CameraOrientationEventListener.cs
+++ Sources/Steepshot/Steepshot.Android/Utils/CameraOrientationEventListener.cs
@@ -1,4 +1,5 @@
-﻿using Android.Content;
+﻿using System;
+using Android.Content;
 using Android.Hardware;
 using Android.Runtime;
 using Android.Views;
@@ -8,25 +9,23 @@ namespace Steepshot.Utils
     public class CameraOrientationEventListener : OrientationEventListener
     {
         public int Orientation;
-        public int CameraId;
-        // 15-degree increments 
-        private static readonly int[] normalLookup = new [] { 0, 0, 0, 90, 90, 90, 90, 90, 90, 180, 180, 180, 180, 180, 180, 270, 270, 270, 270, 270, 270, 0, 0, 0 }; 
+        public event Action<int> OrientationChanged;
+        // 15-degree increments
         private static readonly int[] backCameraLookup = new[] { 90, 90, 90, 180, 180, 180, 180, 180, 180, 270, 270, 270, 270, 270, 270, 0, 0, 0, 0, 0, 0, 90, 90, 90 };
-        private static readonly int[] frontCameralookup = new[] { 270, 270, 270, 0, 0, 0, 0, 0, 0, 90, 90, 90, 90, 90, 90, 180, 180, 180, 180, 180, 180, 270, 270, 270 }; 
 
-        public CameraOrientationEventListener(Context context, [GeneratedEnum] SensorDelay rate, int cameraId) : base(context, rate)
+        public CameraOrientationEventListener(Context context, [GeneratedEnum] SensorDelay rate) : base(context, rate)
         {
-            CameraId = cameraId;
         }
 
         public override void OnOrientationChanged(int orientation)
         {
             if (orientation != OrientationUnknown)
             {
-                var newOrientation = CameraId == 0 ? backCameraLookup[orientation / 15] : frontCameralookup[orientation / 15];
+                var newOrientation = backCameraLookup[orientation / 15];
                 if (Orientation != newOrientation)
                 {
                     Orientation = newOrientation;
+                    OrientationChanged?.Invoke(Orientation);
                 }
             }
         }
