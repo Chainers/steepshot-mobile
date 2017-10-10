6812e100814f5cd5586c10c4c4822c9bec1b1951
KorzunAV <KorzunAV@gmail.com>
2017-10-10 08:54:36 +0300

    PreSearchPresenter task refactoring

--- Sources/Steepshot/Steepshot.Core/Presenters/PreSearchPresenter.cs
+++ Sources/Steepshot/Steepshot.Core/Presenters/PreSearchPresenter.cs
@@ -3,149 +3,95 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
-using Steepshot.Core.Models.Common;
 using Steepshot.Core.Models.Requests;
-using Steepshot.Core.Models.Responses;
-using Steepshot.Core.Utils;
 
 namespace Steepshot.Core.Presenters
 {
-    public class PreSearchPresenter : BaseFeedPresenter
+    public sealed class PreSearchPresenter : BaseFeedPresenter
     {
-        private CancellationTokenSource _cts;
         public PostType PostType = PostType.Top;
-        private bool _hasItems = true;
-        public bool HasItems
-        {
-            get
-            {
-                return _hasItems;
-            }
-        }
-        private string _offsetUrl = string.Empty;
-        private const int PostsCount = 19;
+        private const int ItemsLimit = 20;
         public string Tag;
 
-        public void ClearPosts()
+        public async Task<List<string>> TryLoadNextTopPosts(bool clearOld = false)
         {
-            Posts.Clear();
-            _hasItems = true;
-            _offsetUrl = string.Empty;
-        }
-
-        public async Task<List<string>> GetTopPosts(bool clearOld = false)
-        {
-            List<string> errors = null;
-            try
-            {
-                if (clearOld)
-                    ClearPosts();
-                if (!_hasItems)
-                    return errors;
-                try
-                {
-                    _cts?.Cancel();
-                }
-                catch (ObjectDisposedException)
-                {
-
-                }
-
-                using (_cts = new CancellationTokenSource())
-                {
-                    var postrequest = new PostsRequest(PostType)
-                    {
-                        Login = User.Login,
-                        Limit = PostsCount,
-                        Offset = _offsetUrl,
-                        ShowNsfw = User.IsNsfw,
-                        ShowLowRated = User.IsLowRated
-                    };
-                    var response = await Api.GetPosts(postrequest, _cts);
-
-                    errors = response.Errors;
-                    if (response.Success && response.Result?.Results != null)
-                    {
-                        if (response.Result.Results.Count != 0)
-                        {
-                            var lastItem = response.Result.Results.Last();
-                            if (lastItem.Url != _offsetUrl)
-                                response.Result.Results.Remove(lastItem);
-                            else
-                                _hasItems = false;
-
-                            _offsetUrl = lastItem.Url;
-                            Posts.AddRange(response.Result.Results);
-                        }
-                    }
-                }
-            }
-            catch (TaskCanceledException e)
-            {
-                throw e;
-            }
-            catch (Exception ex)
-            {
-                AppSettings.Reporter.SendCrash(ex);
-            }
-            return errors;
-        }
-
-        public async Task<List<string>> GetSearchedPosts(bool clearOld = false)
-        {
-            List<string> errors = null;
             if (clearOld)
                 ClearPosts();
-            if (!_hasItems)
-                return errors;
-            try
-            {
-                _cts?.Cancel();
-            }
-            catch (ObjectDisposedException)
-            {
 
-            }
-            try
+            if (IsLastReaded)
+                return null;
+
+            return await RunAsSingleTask(LoadNextTopPosts);
+        }
+
+        private async Task<List<string>> LoadNextTopPosts(CancellationTokenSource cts)
+        {
+            var postrequest = new PostsRequest(PostType)
             {
-                using (_cts = new CancellationTokenSource())
+                Login = User.Login,
+                Limit = ItemsLimit,
+                Offset = OffsetUrl,
+                ShowNsfw = User.IsNsfw,
+                ShowLowRated = User.IsLowRated
+            };
+            var response = await Api.GetPosts(postrequest, cts);
+
+            if (response.Success)
+            {
+                var posts = response.Result.Results;
+                if (posts.Count > 0)
                 {
-                    var postrequest = new PostsByCategoryRequest(PostType, Tag)
-                    {
-                        Login = User.Login,
-                        Limit = PostsCount,
-                        Offset = _offsetUrl,
-                        ShowNsfw = User.IsNsfw,
-                        ShowLowRated = User.IsLowRated
-                    };
+                    lock (Posts)
+                        Posts.AddRange(string.IsNullOrEmpty(OffsetUrl) ? posts : posts.Skip(1));
 
-                    var posts = await Api.GetPostsByCategory(postrequest, _cts);
-                    errors = posts.Errors;
-                    if (posts.Success && posts.Result?.Results != null)
-                    {
-                        if (posts.Result.Results.Count != 0)
-                        {
-                            var lastItem = posts.Result.Results.Last();
-                            if (lastItem.Url != _offsetUrl)
-                                posts.Result.Results.Remove(lastItem);
-                            else
-                                _hasItems = false;
-
-                            _offsetUrl = lastItem.Url;
-                            Posts.AddRange(posts.Result.Results);
-                        }
-                    }
+                    OffsetUrl = posts.Last().Url;
                 }
+
+                if (posts.Count < Math.Min(ServerMaxCount, ItemsLimit))
+                    IsLastReaded = true;
             }
-            catch (TaskCanceledException e)
+            return response.Errors;
+        }
+
+        public async Task<List<string>> TryGetSearchedPosts(bool clearOld = false)
+        {
+            if (clearOld)
+                ClearPosts();
+
+            if (IsLastReaded)
+                return null;
+
+            return await RunAsSingleTask(GetSearchedPosts);
+        }
+
+        private async Task<List<string>> GetSearchedPosts(CancellationTokenSource cts)
+        {
+            var postrequest = new PostsByCategoryRequest(PostType, Tag)
             {
-                throw e;
-            }
-            catch (Exception ex)
+                Login = User.Login,
+                Limit = ItemsLimit,
+                Offset = OffsetUrl,
+                ShowNsfw = User.IsNsfw,
+                ShowLowRated = User.IsLowRated
+            };
+
+            var response = await Api.GetPostsByCategory(postrequest, cts);
+            if (response.Success)
             {
-                AppSettings.Reporter.SendCrash(ex);
+                var posts = response.Result.Results;
+                if (posts.Count > 0)
+                {
+                    lock (Posts)
+                        Posts.AddRange(string.IsNullOrEmpty(OffsetUrl) ? posts : posts.Skip(1));
+
+                    OffsetUrl = posts.Last().Url;
+                }
+
+                if (posts.Count < Math.Min(ServerMaxCount, ItemsLimit))
+                    IsLastReaded = true;
             }
-            return errors;
+
+            return response.Errors;
         }
     }
 }
