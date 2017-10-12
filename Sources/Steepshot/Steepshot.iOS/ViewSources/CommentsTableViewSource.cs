cebdc04f236619604ce7f4de58ff6d34640bdd3d
Uhnivenka Stanislau <ugnivs@gmail.com>
2017-10-12 11:04:40 +0300

    Flag comments ios

--- Sources/Steepshot/Steepshot.iOS/ViewSources/CommentsTableViewSource.cs
+++ Sources/Steepshot/Steepshot.iOS/ViewSources/CommentsTableViewSource.cs
@@ -6,40 +6,48 @@ using UIKit;
 
 namespace Steepshot.iOS.ViewSources
 {
-	public class CommentsTableViewSource : BaseTableSource<Post>
-	{
-		string _cellIdentifier = nameof(CommentTableViewCell);
-		public event VoteEventHandler<VoteResponse> Voted;
-		public event HeaderTappedHandler GoToProfile;
+    public class CommentsTableViewSource : BaseTableSource<Post>
+    {
+        string _cellIdentifier = nameof(CommentTableViewCell);
+        public event VoteEventHandler<VoteResponse> Voted;
+        public event VoteEventHandler<VoteResponse> Flaged;
+        public event HeaderTappedHandler GoToProfile;
 
-		public override UITableViewCell GetCell(UITableView tableView, NSIndexPath indexPath)
-		{
-			var cell = (CommentTableViewCell)tableView.DequeueReusableCell(_cellIdentifier, indexPath);
-			if (!cell.IsVotedSet)
+        public override UITableViewCell GetCell(UITableView tableView, NSIndexPath indexPath)
+        {
+            var cell = (CommentTableViewCell)tableView.DequeueReusableCell(_cellIdentifier, indexPath);
+            if (!cell.IsVotedSet)
             {
                 cell.Voted += (vote, url, action) =>
                 {
-					Voted(vote, url, action);
+                    Voted(vote, url, action);
                 };
             }
-			if (!cell.IsGoToProfileSet)
-			{
-				cell.GoToProfile += (username) =>
-				{
-					if(GoToProfile != null)
-						GoToProfile(username);
-				};
-			}
-			cell.UpdateCell(TableItems[indexPath.Row]);
-			return cell;
-		}
+            if (!cell.IsFlagedSet)
+            {
+                cell.Flaged += (vote, postUri, action) =>
+                {
+                    Flaged(vote, postUri, action);
+                };
+            }
+            if (!cell.IsGoToProfileSet)
+            {
+                cell.GoToProfile += (username) =>
+                {
+                    if (GoToProfile != null)
+                        GoToProfile(username);
+                };
+            }
+            cell.UpdateCell(TableItems[indexPath.Row]);
+            return cell;
+        }
 
-		public override void WillDisplay(UITableView tableView, UITableViewCell cell, NSIndexPath indexPath)
-		{
-			/*if (indexPath.Row == TableItems.Count - 1 && ScrolledToBottom != null)
+        public override void WillDisplay(UITableView tableView, UITableViewCell cell, NSIndexPath indexPath)
+        {
+            /*if (indexPath.Row == TableItems.Count - 1 && ScrolledToBottom != null)
 			{
 				ScrolledToBottom();
 			}*/
-		}
-	}
+        }
+    }
 }
