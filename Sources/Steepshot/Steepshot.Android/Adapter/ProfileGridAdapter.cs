d1825e85f973a6da07c36e01ce0c8cbeeb69b2bd
KorzunAV <KorzunAV@gmail.com>
2017-10-27 14:33:32 +0300

    CC

--- Sources/Steepshot/Steepshot.Android/Adapter/ProfileGridAdapter.cs
+++ Sources/Steepshot/Steepshot.Android/Adapter/ProfileGridAdapter.cs
@@ -2,7 +2,6 @@
 using Android.Content;
 using Android.Graphics;
 using Android.Graphics.Drawables;
-using Android.Support.V4.Content;
 using Android.Support.V7.Widget;
 using Android.Views;
 using Android.Widget;
@@ -19,12 +18,8 @@ namespace Steepshot.Adapter
         public Action FollowersAction, FollowingAction, BalanceAction;
         public Action FollowAction;
         public UserProfileResponse ProfileData;
-        private bool _isHeaderNeeded;
+        private readonly bool _isHeaderNeeded;
 
-        public ProfileGridAdapter(Context context, BasePostPresenter presenter, bool isHeaderNeeded = true) : base(context, presenter)
-        {
-            _isHeaderNeeded = isHeaderNeeded;
-        }
 
         public override int ItemCount
         {
@@ -34,6 +29,13 @@ namespace Steepshot.Adapter
                 return count == 0 || Presenter.IsLastReaded ? count + 1 : count + 2;
             }
         }
+
+
+        public ProfileGridAdapter(Context context, BasePostPresenter presenter, bool isHeaderNeeded = true) : base(context, presenter)
+        {
+            _isHeaderNeeded = isHeaderNeeded;
+        }
+
 
         public override void OnBindViewHolder(RecyclerView.ViewHolder holder, int position)
         {
@@ -75,26 +77,26 @@ namespace Steepshot.Adapter
 
     public class HeaderViewHolder : RecyclerView.ViewHolder
     {
-        private Context _context;
+        private readonly Context _context;
 
-        private TextView _name;
-        private TextView _place;
-        private TextView _description;
-        private TextView _site;
-        private TextView _photos_count;
-        private TextView _photos_title;
-        private TextView _following_count;
-        private TextView _following_title;
-        private TextView _followers_count;
-        private TextView _followers_title;
-        private TextView _balance_text;
-        private TextView _balance;
+        private readonly TextView _name;
+        private readonly TextView _place;
+        private readonly TextView _description;
+        private readonly TextView _site;
+        private readonly TextView _photosCount;
+        private readonly TextView _photosTitle;
+        private readonly TextView _followingCount;
+        private readonly TextView _followingTitle;
+        private readonly TextView _followersCount;
+        private readonly TextView _followersTitle;
+        private readonly TextView _balanceText;
+        private readonly TextView _balance;
 
-        private Refractored.Controls.CircleImageView _profile_image;
-        private LinearLayout _following_btn;
-        private LinearLayout _followers_btn;
-        private RelativeLayout _balance_container;
-        private Button _follow_button;
+        private readonly Refractored.Controls.CircleImageView _profileImage;
+        private readonly LinearLayout _followingBtn;
+        private readonly LinearLayout _followersBtn;
+        private readonly RelativeLayout _balanceContainer;
+        private readonly Button _followButton;
         private View _isamage;
 
         private readonly Action _followersAction, _followingAction, _followAction, _balanceAction;
@@ -107,20 +109,20 @@ namespace Steepshot.Adapter
             _place = itemView.FindViewById<TextView>(Resource.Id.place);
             _description = itemView.FindViewById<TextView>(Resource.Id.description);
             _site = itemView.FindViewById<TextView>(Resource.Id.site);
-            _photos_count = itemView.FindViewById<TextView>(Resource.Id.photos_count);
-            _photos_title = itemView.FindViewById<TextView>(Resource.Id.photos_title);
-            _following_count = itemView.FindViewById<TextView>(Resource.Id.following_count);
-            _following_title = itemView.FindViewById<TextView>(Resource.Id.following_title);
-            _followers_count = itemView.FindViewById<TextView>(Resource.Id.followers_count);
-            _followers_title = itemView.FindViewById<TextView>(Resource.Id.followers_title);
-            _balance_text = itemView.FindViewById<TextView>(Resource.Id.balance_text);
+            _photosCount = itemView.FindViewById<TextView>(Resource.Id.photos_count);
+            _photosTitle = itemView.FindViewById<TextView>(Resource.Id.photos_title);
+            _followingCount = itemView.FindViewById<TextView>(Resource.Id.following_count);
+            _followingTitle = itemView.FindViewById<TextView>(Resource.Id.following_title);
+            _followersCount = itemView.FindViewById<TextView>(Resource.Id.followers_count);
+            _followersTitle = itemView.FindViewById<TextView>(Resource.Id.followers_title);
+            _balanceText = itemView.FindViewById<TextView>(Resource.Id.balance_text);
             _balance = itemView.FindViewById<TextView>(Resource.Id.balance);
 
-            _profile_image = itemView.FindViewById<Refractored.Controls.CircleImageView>(Resource.Id.profile_image);
-            _following_btn = itemView.FindViewById<LinearLayout>(Resource.Id.following_btn);
-            _followers_btn = itemView.FindViewById<LinearLayout>(Resource.Id.followers_btn);
-            _balance_container = itemView.FindViewById<RelativeLayout>(Resource.Id.balance_container);
-            _follow_button = itemView.FindViewById<Button>(Resource.Id.follow_button);
+            _profileImage = itemView.FindViewById<Refractored.Controls.CircleImageView>(Resource.Id.profile_image);
+            _followingBtn = itemView.FindViewById<LinearLayout>(Resource.Id.following_btn);
+            _followersBtn = itemView.FindViewById<LinearLayout>(Resource.Id.followers_btn);
+            _balanceContainer = itemView.FindViewById<RelativeLayout>(Resource.Id.balance_container);
+            _followButton = itemView.FindViewById<Button>(Resource.Id.follow_button);
 
             //_isamage = itemView.FindViewById<View>(Resource.Id.follow_button_solid);
 
@@ -128,13 +130,13 @@ namespace Steepshot.Adapter
             _place.Typeface = Style.Regular;
             _description.Typeface = Style.Regular;
             _site.Typeface = Style.Regular;
-            _photos_count.Typeface = Style.Semibold;
-            _photos_title.Typeface = Style.Regular;
-            _following_count.Typeface = Style.Semibold;
-            _following_title.Typeface = Style.Regular;
-            _followers_count.Typeface = Style.Semibold;
-            _followers_title.Typeface = Style.Regular;
-            _balance_text.Typeface = Style.Regular;
+            _photosCount.Typeface = Style.Semibold;
+            _photosTitle.Typeface = Style.Regular;
+            _followingCount.Typeface = Style.Semibold;
+            _followingTitle.Typeface = Style.Regular;
+            _followersCount.Typeface = Style.Semibold;
+            _followersTitle.Typeface = Style.Regular;
+            _balanceText.Typeface = Style.Regular;
             _balance.Typeface = Style.Regular;
 
             _followersAction = followersAction;
@@ -142,88 +144,115 @@ namespace Steepshot.Adapter
             _balanceAction = balanceAction;
             _followAction = followAction;
 
-            _following_btn.Click += (sender, e) => { _followingAction?.Invoke(); };
-            _followers_btn.Click += (sender, e) => { _followersAction?.Invoke(); };
-            _balance_container.Click += (sender, e) => { _balanceAction?.Invoke(); };
-            _follow_button.Click += (sender, e) => { _followAction?.Invoke(); };
+            _followingBtn.Click += OnFollowingBtnOnClick;
+            _followersBtn.Click += OnFollowersBtnOnClick;
+            _balanceContainer.Click += OnBalanceContainerOnClick;
+            _followButton.Click += OnFollowButtonOnClick;
         }
 
-        public void UpdateHeader(UserProfileResponse _profile)
+        private void OnFollowButtonOnClick(object sender, EventArgs e)
         {
-            if (_profile != null)
+            _followAction?.Invoke();
+        }
+
+        private void OnBalanceContainerOnClick(object sender, EventArgs e)
+        {
+            _balanceAction?.Invoke();
+        }
+
+        private void OnFollowersBtnOnClick(object sender, EventArgs e)
+        {
+            _followersAction?.Invoke();
+        }
+
+        private void OnFollowingBtnOnClick(object sender, EventArgs e)
+        {
+            _followingAction?.Invoke();
+        }
+
+        public void UpdateHeader(UserProfileResponse profile)
+        {
+            if (profile == null)
+                return;
+
+            var siteTextColor = Style.R231G72B00;
+            if (!string.IsNullOrEmpty(profile.ProfileImage))
             {
-                var blackTextColor = BitmapUtils.GetColorFromInteger(ContextCompat.GetColor(_context, Resource.Color.rgb15_24_30));
-                var siteTextColor = BitmapUtils.GetColorFromInteger(ContextCompat.GetColor(_context, Resource.Color.rgb231_72_0));
-                if (!string.IsNullOrEmpty(_profile?.ProfileImage))
-                {
-                    Picasso.With(_context).Load(_profile?.ProfileImage)
-                           .Resize(300, 300)
-                           .CenterCrop()
-                           .Into(_profile_image);
-                }
-                else
-                    _profile_image.SetImageResource(Resource.Drawable.holder);
-
-                if (BasePresenter.User.Login == _profile.Username)
-                    _follow_button.Visibility = ViewStates.Gone;
-                else if (_profile.HasFollowed)
-                {
-                    var background = (GradientDrawable)_follow_button.Background;
-                    background.SetColor(Color.White);
-                    background.SetStroke(1, BitmapUtils.GetColorFromInteger(ContextCompat.GetColor(_context, Resource.Color.rgb244_244_246)));
-                    _follow_button.Text = Localization.Messages.Unfollow;
-                    _follow_button.SetTextColor(BitmapUtils.GetColorFromInteger(ContextCompat.GetColor(_context, Resource.Color.rgb15_24_30)));
-                }
-                else
-                {
-                    var background = (GradientDrawable)_follow_button.Background;
-                    background.SetColor(BitmapUtils.GetColorFromInteger(ContextCompat.GetColor(_context, Resource.Color.rgb231_72_0)));
-                    background.SetStroke(0, Color.White);
-                    _follow_button.Text = Localization.Messages.Follow;
-                    _follow_button.SetTextColor(Color.White);
-                }
-
-                if (!string.IsNullOrEmpty(_profile?.Name))
-                {
-                    _name.Text = _profile?.Name;
-                    _name.SetTextColor(blackTextColor);
-                }
-                else if (BasePresenter.User.Login != _profile.Username)
-                    _name.Visibility = ViewStates.Gone;
-
-                if (!string.IsNullOrEmpty(_profile?.Location))
-                    _place.Text = _profile.Location.Trim();
-                else if (BasePresenter.User.Login != _profile.Username)
-                    _place.Visibility = ViewStates.Gone;
-
-                if (!string.IsNullOrEmpty(_profile?.About))
-                {
-                    _description.Text = _profile.About;
-                    _description.SetTextColor(blackTextColor);
-                }
-                else if (BasePresenter.User.Login != _profile.Username)
-                    _description.Visibility = ViewStates.Gone;
-
-                if (!string.IsNullOrEmpty(_profile?.Website))
-                {
-                    _site.Text = _profile.Website;
-                    _site.SetTextColor(siteTextColor);
-                }
-                else if (BasePresenter.User.Login != _profile.Username)
-                    _site.Visibility = ViewStates.Gone;
-
-                _photos_count.Text = _profile.PostCount.ToString("N0");
-                _following_count.Text = _profile.FollowingCount.ToString("#,##0");
-                _followers_count.Text = _profile.FollowersCount.ToString("#,##0");
-
-                _balance.Text = BasePresenter.ToFormatedCurrencyString(_profile.EstimatedBalance);
+                Picasso.With(_context).Load(profile.ProfileImage)
+                    .Resize(300, 300)
+                    .CenterCrop()
+                    .Into(_profileImage);
             }
+            else
+                _profileImage.SetImageResource(Resource.Drawable.holder);
+
+            if (string.Equals(BasePresenter.User.Login, profile.Username, StringComparison.OrdinalIgnoreCase))
+            {
+                _followButton.Visibility = ViewStates.Gone;
+            }
+            else if (profile.HasFollowed)
+            {
+                var background = (GradientDrawable)_followButton.Background;
+                background.SetColor(Color.White);
+                background.SetStroke(1, Style.R244G244B246);
+                _followButton.Text = Localization.Messages.Unfollow;
+                _followButton.SetTextColor(Style.R15G24B30);
+            }
+            else
+            {
+                var background = (GradientDrawable)_followButton.Background;
+                background.SetColor(Style.R231G72B00);
+                background.SetStroke(0, Color.White);
+                _followButton.Text = Localization.Messages.Follow;
+                _followButton.SetTextColor(Color.White);
+            }
+
+            if (!string.IsNullOrEmpty(profile.Name))
+            {
+                _name.Text = profile.Name;
+                _name.SetTextColor(Style.R15G24B30);
+            }
+            else if (!string.Equals(BasePresenter.User.Login, profile.Username, StringComparison.OrdinalIgnoreCase))
+            {
+                _name.Visibility = ViewStates.Gone;
+            }
+
+            if (!string.IsNullOrEmpty(profile.Location))
+                _place.Text = profile.Location.Trim();
+            else if (!string.Equals(BasePresenter.User.Login, profile.Username, StringComparison.OrdinalIgnoreCase))
+                _place.Visibility = ViewStates.Gone;
+
+            if (!string.IsNullOrEmpty(profile.About))
+            {
+                _description.Text = profile.About;
+                _description.SetTextColor(Style.R15G24B30);
+            }
+            else if (!string.Equals(BasePresenter.User.Login, profile.Username, StringComparison.OrdinalIgnoreCase))
+            {
+                _description.Visibility = ViewStates.Gone;
+            }
+
+            if (!string.IsNullOrEmpty(profile.Website))
+            {
+                _site.Text = profile.Website;
+                _site.SetTextColor(siteTextColor);
+            }
+            else if (!string.Equals(BasePresenter.User.Login, profile.Username, StringComparison.OrdinalIgnoreCase))
+            {
+                _site.Visibility = ViewStates.Gone;
+            }
+
+            _photosCount.Text = profile.PostCount.ToString("N0");
+            _followingCount.Text = profile.FollowingCount.ToString("#,##0");
+            _followersCount.Text = profile.FollowersCount.ToString("#,##0");
+
+            _balance.Text = BasePresenter.ToFormatedCurrencyString(profile.EstimatedBalance);
         }
     }
 
     public class ProfileImageViewHolder : ImageViewHolder
     {
-        private bool _isHeaderNeeded;
+        private readonly bool _isHeaderNeeded;
 
         public ProfileImageViewHolder(View itemView, Action<int> click, bool isHeaderNeeded) : base(itemView, click)
         {
--- Sources/Steepshot/Steepshot.Android/Utils/Style.cs
+++ Sources/Steepshot/Steepshot.Android/Utils/Style.cs
@@ -1,5 +1,6 @@
 ﻿using Android.App;
 using Android.Graphics;
+using Android.Support.V4.Content;
 
 namespace Steepshot.Utils
 {
@@ -8,12 +9,19 @@ namespace Steepshot.Utils
         public static readonly Typeface Regular;
         public static readonly Typeface Semibold;
 
+        public static readonly Color R15G24B30;
+        public static readonly Color R151G155B158;
+        public static readonly Color R244G244B246;
+        public static readonly Color R231G72B00;
 
         static Style()
         {
             Regular = Typeface.CreateFromAsset(Application.Context.Assets, "OpenSans-Regular.ttf");
             Semibold = Typeface.CreateFromAsset(Application.Context.Assets, "OpenSans-Semibold.ttf");
+            R15G24B30 = BitmapUtils.GetColorFromInteger(ContextCompat.GetColor(Application.Context, Resource.Color.rgb15_24_30));
+            R151G155B158 = BitmapUtils.GetColorFromInteger(ContextCompat.GetColor(Application.Context, Resource.Color.rgb151_155_158));
+            R244G244B246 = BitmapUtils.GetColorFromInteger(ContextCompat.GetColor(Application.Context, Resource.Color.rgb244_244_246));
+            R231G72B00 = BitmapUtils.GetColorFromInteger(ContextCompat.GetColor(Application.Context, Resource.Color.rgb231_72_0));
         }
-
     }
 }
