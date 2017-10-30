51cc028e050727a3dce7fea12f666ae691fc7414
KorzunAV <KorzunAV@gmail.com>
2017-10-30 09:27:58 +0300

    CC

--- Sources/Steepshot/Steepshot.Android/Fragment/BackStackFragment.cs
+++ Sources/Steepshot/Steepshot.Android/Fragment/BackStackFragment.cs
@@ -6,32 +6,33 @@ namespace Steepshot.Fragment
     {
         public bool HandleBackPressed(FragmentManager fm)
         {
-            if (fm?.Fragments != null)
+            if (fm?.Fragments == null)
+                return false;
+
+            foreach (var frag in fm.Fragments)
             {
-                foreach (var frag in fm.Fragments)
+                var backStack = frag as BackStackFragment;
+                if (backStack != null && backStack.IsVisible)
                 {
-                    var backStack = frag as BackStackFragment;
-                    if (backStack != null && backStack.IsVisible)
-                    {
-                        if (backStack.OnBackPressed())
-                        {
-                            return true;
-                        }
-                    }
+                    if (backStack.OnBackPressed())
+                        return true;
                 }
             }
+
             return false;
         }
 
-        protected bool OnBackPressed()
+        private bool OnBackPressed()
         {
             if (HandleBackPressed(ChildFragmentManager))
                 return true;
-            else if (UserVisibleHint && ChildFragmentManager.BackStackEntryCount > 0)
+
+            if (UserVisibleHint && ChildFragmentManager.BackStackEntryCount > 0)
             {
                 ChildFragmentManager.PopBackStack();
                 return true;
             }
+
             return false;
         }
     }
