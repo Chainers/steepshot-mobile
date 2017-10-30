2e7c6cb9f9a2455d5fcbc85aca273eb5244903b0
KorzunAV <KorzunAV@gmail.com>
2017-10-30 12:53:44 +0300

    СС

--- Sources/Steepshot/Steepshot.Android/Fragment/HostFragment.cs
+++ Sources/Steepshot/Steepshot.Android/Fragment/HostFragment.cs
@@ -7,34 +7,6 @@ namespace Steepshot.Fragment
     {
         private Android.Support.V4.App.Fragment _fragment;
 
-        public override Android.Views.View OnCreateView(Android.Views.LayoutInflater inflater, Android.Views.ViewGroup container, Android.OS.Bundle savedInstanceState)
-        {
-            base.OnCreateView(inflater, container, savedInstanceState);
-            var view = inflater.Inflate(Resource.Layout.HostLayout, container, false);
-            if (_fragment != null)
-            {
-                ReplaceFragment(_fragment, false);
-            }
-            return view;
-        }
-
-        public void ReplaceFragment(Android.Support.V4.App.Fragment fragment, bool addToBackstack)
-        {
-            if (addToBackstack)
-            {
-                ChildFragmentManager.BeginTransaction().Replace(Resource.Id.child_fragment_container, fragment).AddToBackStack(null).Commit();
-            }
-            else
-            {
-                ChildFragmentManager.BeginTransaction().Replace(Resource.Id.child_fragment_container, fragment).Commit();
-            }
-        }
-
-        public static HostFragment NewInstance(Android.Support.V4.App.Fragment fragment)
-        {
-            return new HostFragment { _fragment = fragment };
-        }
-
         public override bool UserVisibleHint
         {
             get => base.UserVisibleHint;
@@ -44,6 +16,34 @@ namespace Steepshot.Fragment
                     bf.CustomUserVisibleHint = value;
                 base.UserVisibleHint = value;
             }
+        }
+
+
+        public override Android.Views.View OnCreateView(Android.Views.LayoutInflater inflater, Android.Views.ViewGroup container, Android.OS.Bundle savedInstanceState)
+        {
+            base.OnCreateView(inflater, container, savedInstanceState);
+            var view = inflater.Inflate(Resource.Layout.HostLayout, container, false);
+
+            if (_fragment != null)
+                ReplaceFragment(_fragment, false);
+
+            return view;
+        }
+
+        public void ReplaceFragment(Android.Support.V4.App.Fragment fragment, bool addToBackstack)
+        {
+            var transaction = ChildFragmentManager.BeginTransaction();
+            transaction.Replace(Resource.Id.child_fragment_container, fragment);
+
+            if (addToBackstack)
+                transaction.AddToBackStack(null);
+
+            transaction.Commit();
+        }
+
+        public static HostFragment NewInstance(Android.Support.V4.App.Fragment fragment)
+        {
+            return new HostFragment { _fragment = fragment };
         }
     }
 }
