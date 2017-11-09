6ebb78488af4264f664d3934c65a0b55358fa08e
KorzunAV <KorzunAV@gmail.com>
2017-11-09 13:07:47 +0300

    added models compatibility tests (server responses)

--- Sources/Steepshot/Steepshot.Core.Tests/ServerResponceTests.cs
+++ Sources/Steepshot/Steepshot.Core.Tests/ServerResponceTests.cs
@@ -1,23 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Configuration;
-using System.Linq;
-using System.Net;
-using System.Reflection;
-using System.Text;
-using System.Text.RegularExpressions;
 using System.Threading;
 using System.Threading.Tasks;
-using Newtonsoft.Json;
 using NUnit.Framework;
-using RestSharp.Portable;
 using Steepshot.Core.HttpClient;
-using Steepshot.Core.Models.Common;
 using Steepshot.Core.Models.Requests;
-using Steepshot.Core.Models.Responses;
-using Steepshot.Core.Serializing;
-using Newtonsoft.Json.Linq;
 using Steepshot.Core.Authority;
+using Steepshot.Core.Tests.Stubs;
 
 namespace Steepshot.Core.Tests
 {
@@ -26,15 +16,14 @@ namespace Steepshot.Core.Tests
     {
         private const bool IsDev = false;
         private static readonly Dictionary<string, UserInfo> Users;
-        private static readonly Dictionary<string, ApiGateway> Gateway;
-        private static readonly JsonNetConverter JsonConverter;
+        private static readonly Dictionary<string, BaseClient> Gateway;
 
         static ServerResponceTests()
         {
-            Gateway = new Dictionary<string, ApiGateway>
+            Gateway = new Dictionary<string, BaseClient>
             {
-                {"Steem", new ApiGateway(IsDev ? Constants.SteemUrlQa : Constants.SteemUrl)},
-                {"Golos", new ApiGateway(IsDev ? Constants.GolosUrlQa : Constants.GolosUrl)},
+                {"Steem", new AssertedBaseClient(IsDev ? Constants.SteemUrlQa : Constants.SteemUrl)},
+                {"Golos", new AssertedBaseClient(IsDev ? Constants.GolosUrlQa : Constants.GolosUrl)},
             };
 
             Users = new Dictionary<string, UserInfo>
@@ -42,8 +31,6 @@ namespace Steepshot.Core.Tests
                 {"Steem", new UserInfo {Login = "joseph.kalu", PostingKey = ConfigurationManager.AppSettings["SteemWif"]}},
                 {"Golos", new UserInfo {Login = "joseph.kalu", PostingKey = ConfigurationManager.AppSettings["GolosWif"]}}
             };
-
-            JsonConverter = new JsonNetConverter();
         }
 
         [Test]
@@ -57,17 +44,8 @@ namespace Steepshot.Core.Tests
                 ShowLowRated = true
             };
 
-            var parameters = new Dictionary<string, object>();
-            AddOffsetLimitParameters(parameters, request.Offset, request.Limit);
-            AddLoginParameter(parameters, request.Login);
-            AddCensorParameters(parameters, request);
-
-            var endpoint = $"user/{request.Username}/posts";
-
-            var response = await Gateway[apiName].Get(GatewayVersion.V1P1, endpoint, parameters, CancellationToken.None);
-            var errorResult = CheckErrors(response);
-
-            TestResponce<UserPostResponse>(response?.Content, errorResult);
+            var errors = await Gateway[apiName].GetUserPosts(request, CancellationToken.None);
+            Assert.IsTrue(errors.Success, string.Join(Environment.NewLine, errors.Errors));
         }
 
         [Test]
@@ -81,17 +59,8 @@ namespace Steepshot.Core.Tests
                 ShowNsfw = true
             };
 
-            var parameters = new Dictionary<string, object>();
-            AddOffsetLimitParameters(parameters, request.Offset, request.Limit);
-            AddLoginParameter(parameters, request.Login);
-            AddCensorParameters(parameters, request);
-
-            var endpoint = "recent";
-
-            var response = await Gateway[apiName].Get(GatewayVersion.V1P1, endpoint, parameters, CancellationToken.None);
-            var errorResult = CheckErrors(response);
-
-            TestResponce<UserPostResponse>(response?.Content, errorResult);
+            var errors = await Gateway[apiName].GetUserRecentPosts(request, CancellationToken.None);
+            Assert.IsTrue(errors.Success, string.Join(Environment.NewLine, errors.Errors));
         }
 
         [Test]
@@ -99,35 +68,16 @@ namespace Steepshot.Core.Tests
         {
             var request = new PostsRequest(PostType.Top);
 
-            var parameters = new Dictionary<string, object>();
-            AddOffsetLimitParameters(parameters, request.Offset, request.Limit);
-            AddLoginParameter(parameters, request.Login);
-            AddCensorParameters(parameters, request);
-
-            var endpoint = $"posts/{request.Type.ToString().ToLowerInvariant()}";
-
-            var response = await Gateway[apiName].Get(GatewayVersion.V1P1, endpoint, parameters, CancellationToken.None);
-            var errorResult = CheckErrors(response);
-
-            TestResponce<UserPostResponse>(response?.Content, errorResult);
+            var errors = await Gateway[apiName].GetPosts(request, CancellationToken.None);
+            Assert.IsTrue(errors.Success, string.Join(Environment.NewLine, errors.Errors));
         }
 
         [Test, Sequential]
         public async Task GetPostsByCategoryTest([Values("Steem", "Golos")] string apiName, [Values("food", "ru--golos")] string category)
         {
             var request = new PostsByCategoryRequest(PostType.Top, category);
-
-            var parameters = new Dictionary<string, object>();
-            AddOffsetLimitParameters(parameters, request.Offset, request.Limit);
-            AddLoginParameter(parameters, request.Login);
-            AddCensorParameters(parameters, request);
-
-            var endpoint = $"posts/{request.Category}/{request.Type.ToString().ToLowerInvariant()}";
-
-            var response = await Gateway[apiName].Get(GatewayVersion.V1P1, endpoint, parameters, CancellationToken.None);
-            var errorResult = CheckErrors(response);
-
-            TestResponce<UserPostResponse>(response?.Content, errorResult);
+            var errors = await Gateway[apiName].GetPostsByCategory(request, CancellationToken.None);
+            Assert.IsTrue(errors.Success, string.Join(Environment.NewLine, errors.Errors));
         }
 
         [Test, Sequential]
@@ -139,165 +89,82 @@ namespace Steepshot.Core.Tests
                 Offset = string.Empty,
 
             };
-            var parameters = new Dictionary<string, object>();
-            AddOffsetLimitParameters(parameters, request.Offset, request.Limit);
-            if (!string.IsNullOrEmpty(request.Login))
-                AddLoginParameter(parameters, request.Login);
 
-            var endpoint = $"post/{request.Url}/voters";
-
-            var response = await Gateway[apiName].Get(GatewayVersion.V1P1, endpoint, parameters, CancellationToken.None);
-            var errorResult = CheckErrors(response);
-
-            TestResponce<SearchResponse<UserFriend>>(response?.Content, errorResult);
+            var errors = await Gateway[apiName].GetPostVoters(request, CancellationToken.None);
+            Assert.IsTrue(errors.Success, string.Join(Environment.NewLine, errors.Errors));
         }
 
         [Test, Sequential]
         public async Task GetCommentsTest([Values("Steem", "Golos")] string apiName, [Values("@joseph.kalu/cat636203355240074655", "@joseph.kalu/cat636281384922864910")] string url)
         {
             var request = new NamedInfoRequest(url);
-
-            var parameters = new Dictionary<string, object>();
-            AddOffsetLimitParameters(parameters, request.Offset, request.Limit);
-            AddLoginParameter(parameters, request.Login);
-
-            var endpoint = $"post/{request.Url}/comments";
-
-            var response = await Gateway[apiName].Get(GatewayVersion.V1P1, endpoint, parameters, CancellationToken.None);
-            var errorResult = CheckErrors(response);
-
-
-            TestResponce<UserPostResponse>(response?.Content, errorResult);
+            var errors = await Gateway[apiName].GetComments(request, CancellationToken.None);
+            Assert.IsTrue(errors.Success, string.Join(Environment.NewLine, errors.Errors));
         }
 
-
-
-
-
-
-
-        private void TestResponce<T>(string json, OperationResult error)
+        [Test]
+        public async Task GetUserProfileTest([Values("Steem", "Golos")] string apiName)
         {
-            if (!error.Success)
-                Assert.Fail(string.Join(Environment.NewLine, error.Errors));
-
-
-            var jObject = JsonConverter.Deserialize<JObject>(json);
-            var type = typeof(T);
-            var propNames = GetPropertyNames(type);
-
-            var chSet = jObject.Children();
-
-            List<string> msg = new List<string>();
-            foreach (var jtoken in chSet)
-            {
-                var tName = ToTitleCase(jtoken.Path);
-                if (!propNames.Contains(tName))
-                {
-                    msg.Add($"Missing {jtoken.Path}");
-                }
-            }
-
-            if (msg.Any())
-            {
-                Assert.Fail($"Some properties ({msg.Count}) was missed! {Environment.NewLine} {string.Join(Environment.NewLine, msg)}");
-            }
+            var user = Users[apiName];
+            var request = new UserProfileRequest(user.Login);
+            var errors = await Gateway[apiName].GetUserProfile(request, CancellationToken.None);
+            Assert.IsTrue(errors.Success, string.Join(Environment.NewLine, errors.Errors));
         }
 
-        private HashSet<string> GetPropertyNames(Type type)
+
+        [Test]
+        public async Task GetUserFriendsTest([Values("Steem", "Golos")] string apiName)
         {
-            var props = type.GetRuntimeProperties();
-            var resp = new HashSet<string>();
-            foreach (var prop in props)
-            {
-                var order = prop.GetCustomAttribute<JsonPropertyAttribute>();
-                if (order != null)
-                {
-                    resp.Add(order.PropertyName);
-                }
-                else
-                {
-                    resp.Add(prop.Name);
-                }
-            }
-            return resp;
+            var user = Users[apiName];
+            var request = new UserFriendsRequest(user.Login, FriendsType.Following);
+            var errors = await Gateway[apiName].GetUserFriends(request, CancellationToken.None);
+            Assert.IsTrue(errors.Success, string.Join(Environment.NewLine, errors.Errors));
         }
 
-        public static string ToTitleCase(string name, bool firstUpper = true)
+        [Test, Sequential]
+        public async Task GetPostInfoTest([Values("Steem", "Golos")] string apiName,
+                [Values("/steepshot/@joseph.kalu/cat636416737569422613-2017-09-22-10-42-38", "/steepshot/@joseph.kalu/cat636416737747907631-2017-09-22-10-42-56")] string url)
         {
-            var sb = new StringBuilder(name);
-            for (var i = 0; i < sb.Length; i++)
+            var request = new NamedInfoRequest(url)
             {
-                if (i == 0 && firstUpper)
-                    sb[i] = char.ToUpper(sb[i]);
-
-                if (sb[i] == '_' && i + 1 < sb.Length)
-                    sb[i + 1] = char.ToUpper(sb[i + 1]);
-            }
-            sb.Replace("_", string.Empty);
-            var rez = sb.ToString();
-
-            return rez;
+                ShowNsfw = true,
+                ShowLowRated = true
+            };
+            var errors = await Gateway[apiName].GetPostInfo(request, CancellationToken.None);
+            Assert.IsTrue(errors.Success, string.Join(Environment.NewLine, errors.Errors));
         }
 
-
-        private void AddOffsetLimitParameters(Dictionary<string, object> parameters, string offset, int limit)
+        [Test]
+        public async Task SearchUserTest([Values("Steem", "Golos")] string apiName)
         {
-            if (!string.IsNullOrWhiteSpace(offset))
-                parameters.Add("offset", offset);
-
-            if (limit > 0)
-                parameters.Add("limit", limit);
+            var request = new SearchWithQueryRequest("aar");
+            var errors = await Gateway[apiName].SearchUser(request, CancellationToken.None);
+            Assert.IsTrue(errors.Success, string.Join(Environment.NewLine, errors.Errors));
         }
 
-        private void AddLoginParameter(Dictionary<string, object> parameters, string login)
+        [Test]
+        public async Task UserExistsCheckTest([Values("Steem", "Golos")] string apiName)
         {
-            if (!string.IsNullOrEmpty(login))
-                parameters.Add("username", login);
+            var user = Users[apiName];
+            var request = new UserExistsRequests(user.Login);
+            var errors = await Gateway[apiName].UserExistsCheck(request, CancellationToken.None);
+            Assert.IsTrue(errors.Success, string.Join(Environment.NewLine, errors.Errors));
         }
 
-        private void AddCensorParameters(Dictionary<string, object> parameters, CensoredNamedRequestWithOffsetLimitFields request)
+        [Test]
+        public async Task GetCategoriesTest([Values("Steem", "Golos")] string apiName)
         {
-            parameters.Add("show_nsfw", Convert.ToInt32(request.ShowNsfw));
-            parameters.Add("show_low_rated", Convert.ToInt32(request.ShowLowRated));
+            var request = new OffsetLimitFields();
+            var errors = await Gateway[apiName].GetCategories(request, CancellationToken.None);
+            Assert.IsTrue(errors.Success, string.Join(Environment.NewLine, errors.Errors));
         }
 
-        protected OperationResult CheckErrors(IRestResponse response)
+        [Test]
+        public async Task SearchCategoriesTest([Values("Steem", "Golos")] string apiName)
         {
-            var result = new OperationResult();
-            var content = response.Content;
-
-            // HTTP errors
-            if (response.StatusCode == HttpStatusCode.BadRequest ||
-                response.StatusCode == HttpStatusCode.Forbidden)
-            {
-                var dic = JsonConverter.Deserialize<Dictionary<string, List<string>>>(content);
-                foreach (var kvp in dic)
-                {
-                    result.Errors.AddRange(kvp.Value);
-                }
-            }
-            else if (response.StatusCode != HttpStatusCode.OK &&
-                     response.StatusCode != HttpStatusCode.Created)
-            {
-                result.Errors.Add(response.StatusDescription);
-            }
-
-            if (!result.Success)
-            {
-                // Checking content
-                if (string.IsNullOrWhiteSpace(content))
-                {
-                    result.Errors.Add(Localization.Errors.EmptyResponseContent);
-                }
-                else if (new Regex(@"<[^>]+>").IsMatch(content))
-                {
-                    result.Errors.Add(Localization.Errors.ResponseContentContainsHtml + content);
-                }
-            }
-
-            return result;
+            var request = new SearchWithQueryRequest("ru");
+            var errors = await Gateway[apiName].SearchCategories(request, CancellationToken.None);
+            Assert.IsTrue(errors.Success, string.Join(Environment.NewLine, errors.Errors));
         }
-
     }
 }
---
+++ Sources/Steepshot/Steepshot.Core.Tests/Stubs/AssertedBaseClient.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Steepshot.Core.HttpClient;
+using Steepshot.Core.Models.Common;
+
+namespace Steepshot.Core.Tests.Stubs
+{
+    public class AssertedBaseClient : BaseClient
+    {
+        public AssertedBaseClient(string url)
+        {
+            Gateway = new ApiGateway(url);
+            EnableRead = true;
+        }
+
+        protected override OperationResult<T> CreateResult<T>(string json, OperationResult error)
+        {
+            if (error.Success)
+            {
+                var jObject = _jsonConverter.Deserialize<JObject>(json);
+                var type = typeof(T);
+                var mNames = GetPropertyNames(type);
+                var jNames = jObject.Children().Select(jtoken => ToTitleCase(jtoken.Path)).ToArray();
+
+                var msg = new List<string>();
+                foreach (var pName in jNames)
+                {
+                    if (!mNames.Contains(pName))
+                    {
+                        msg.Add($"Missing in model {pName}");
+                    }
+                }
+                foreach (var pName in mNames)
+                {
+                    if (!jNames.Contains(pName))
+                    {
+                        msg.Add($"Missing in json {pName}");
+                    }
+                }
+
+                if (msg.Any())
+                {
+                    error.Errors.Add($"Some properties ({msg.Count}) was missed! {Environment.NewLine} {string.Join(Environment.NewLine, msg)}");
+                }
+            }
+            return base.CreateResult<T>(json, error);
+        }
+
+        private HashSet<string> GetPropertyNames(Type type)
+        {
+            var props = type.GetRuntimeProperties();
+            var resp = new HashSet<string>();
+            foreach (var prop in props)
+            {
+                var order = prop.GetCustomAttribute<JsonPropertyAttribute>();
+                if (order != null)
+                {
+                    resp.Add(order.PropertyName);
+                }
+                else
+                {
+                    resp.Add(prop.Name);
+                }
+            }
+            return resp;
+        }
+
+        private string ToTitleCase(string name, bool firstUpper = true)
+        {
+            var sb = new StringBuilder(name);
+            for (var i = 0; i < sb.Length; i++)
+            {
+                if (i == 0 && firstUpper)
+                    sb[i] = char.ToUpper(sb[i]);
+
+                if (sb[i] == '_' && i + 1 < sb.Length)
+                    sb[i + 1] = char.ToUpper(sb[i + 1]);
+            }
+            sb.Replace("_", string.Empty);
+            var rez = sb.ToString();
+
+            return rez;
+        }
+    }
+}
