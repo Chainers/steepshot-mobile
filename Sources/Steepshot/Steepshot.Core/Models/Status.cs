c45956fe6d58f3b88e95144eda04c4c9cad71b92
Nikita Rakitin <nnn93@mail.ru>
2017-11-13 10:39:46 +0300

    Removed unused field

--- Sources/Steepshot/Steepshot.Core/Models/Status.cs
+++ Sources/Steepshot/Steepshot.Core/Models/Status.cs
@@ -1,21 +1,12 @@
-﻿
-namespace Steepshot.Core.Models
+﻿namespace Steepshot.Core.Models
 {
     public struct Status
     {
         public bool IsChanged { get; set; }
-        public object ChangedObject { get; set; }
 
         public Status(bool isChanged)
         {
             IsChanged = isChanged;
-            ChangedObject = null;
-        }
-
-        public Status(bool isChanged, object obj)
-        {
-            IsChanged = isChanged;
-            ChangedObject = obj;
         }
     }
 }
