bec75826217a6b8385f6d318c175937e11f798c5
KorzunAV <KorzunAV@gmail.com>
2017-11-20 17:43:51 +0300

    Ditch update

--- Sources/Steepshot/Steepshot.Core/HttpClient/GolosClient.cs
+++ Sources/Steepshot/Steepshot.Core/HttpClient/GolosClient.cs
@@ -108,7 +108,7 @@ namespace Steepshot.Core.HttpClient
             return await Task.Run(() =>
             {
                 var op = request.Type == FollowType.Follow
-                    ? new FollowOperation(request.Login, request.Username, DitchFollowType.blog, request.Login)
+                    ? new FollowOperation(request.Login, request.Username, DitchFollowType.Blog, request.Login)
                     : new UnfollowOperation(request.Login, request.Username, request.Login);
                 var resp = _operationManager.BroadcastOperations(keys, ct, op);
 
@@ -134,7 +134,7 @@ namespace Steepshot.Core.HttpClient
 
             return await Task.Run(() =>
             {
-                var op = new FollowOperation(request.Login, "steepshot", DitchFollowType.blog, request.Login);
+                var op = new FollowOperation(request.Login, "steepshot", DitchFollowType.Blog, request.Login);
                 var resp = _operationManager.VerifyAuthority(keys, ct, op);
 
                 var result = new OperationResult<LoginResponse>();
@@ -247,8 +247,7 @@ namespace Steepshot.Core.HttpClient
                     ops = new BaseOperation[]
                     {
                         post,
-                        new BeneficiariesOperation(request.Login, post.Permlink, _operationManager.SbdSymbol,
-                            beneficiaries)
+                        new BeneficiariesOperation(request.Login, post.Permlink, _operationManager.SbdSymbol, beneficiaries)
                     };
                 }
                 else
@@ -282,11 +281,12 @@ namespace Steepshot.Core.HttpClient
                 return string.Empty;
 
 
-            var op = new FollowOperation(request.Login, "steepshot", DitchFollowType.blog, request.Login);
-            var tr = _operationManager.CreateTransaction(DynamicGlobalPropertyApiObj.Default, keys, ct, op);
+            var op = new FollowOperation(request.Login, "steepshot", DitchFollowType.Blog, request.Login);
+            var properties = new DynamicGlobalPropertyObject { HeadBlockId = Hex.ToString(_operationManager.ChainId), Time = DateTime.Now, HeadBlockNumber = 0 };
+            var tr = _operationManager.CreateTransaction(properties, keys, ct, op);
             return JsonConverter.Serialize(tr);
         }
-        
+
         #endregion
     }
 }
