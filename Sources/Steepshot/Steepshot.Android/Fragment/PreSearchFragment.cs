d8a67b3c26d6076a3b599c5499a1e0d92fb16d60
Nikita Rakitin <nnn93@mail.ru>
2017-11-23 15:37:51 +0300

    Show more added

--- Sources/Steepshot/Steepshot.Android/Fragment/PreSearchFragment.cs
+++ Sources/Steepshot/Steepshot.Android/Fragment/PreSearchFragment.cs
@@ -376,7 +376,10 @@ namespace Steepshot.Fragment
 
         private void TagAction(string tag)
         {
-            SearchByTag(tag);
+            if (tag != null)
+                SearchByTag(tag);
+            else
+                _adapter.NotifyDataSetChanged();
         }
 
         private void UserAction(Post post)
