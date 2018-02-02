65a79c251c319ac09f0ae7fc20d2c9f470951f12
Nikita Rakitin <nnn93@mail.ru>
2018-02-02 10:42:00 +0300

    cleaning

--- Sources/Steepshot/Steepshot.iOS/Services/SaverService.cs
+++ Sources/Steepshot/Steepshot.iOS/Services/SaverService.cs
@@ -5,21 +5,21 @@ using Steepshot.Core.Services;
 
 namespace Steepshot.iOS
 {
-	public class SaverService : ISaverService
-	{
-		public T Get<T>(string key) where T : new()
-		{
-			var obj = NSUserDefaults.StandardUserDefaults.StringForKey(key);
-			if (obj == null)
-				return new T();
-			return JsonConvert.DeserializeObject<T>(obj);
-		}
+    public class SaverService : ISaverService
+    {
+        public T Get<T>(string key) where T : new()
+        {
+            var obj = NSUserDefaults.StandardUserDefaults.StringForKey(key);
+            if (obj == null)
+                return new T();
+            return JsonConvert.DeserializeObject<T>(obj);
+        }
 
-		public void Save<T>(string key, T obj)
-		{
-			var objToStore = JsonConvert.SerializeObject(obj, new JsonSerializerSettings { ContractResolver = new DefaultContractResolver() });
-			NSUserDefaults.StandardUserDefaults.SetString(objToStore, key);
+        public void Save<T>(string key, T obj)
+        {
+            var objToStore = JsonConvert.SerializeObject(obj, new JsonSerializerSettings { ContractResolver = new DefaultContractResolver() });
+            NSUserDefaults.StandardUserDefaults.SetString(objToStore, key);
             NSUserDefaults.StandardUserDefaults.Synchronize();
-		}
-	}
+        }
+    }
 }
