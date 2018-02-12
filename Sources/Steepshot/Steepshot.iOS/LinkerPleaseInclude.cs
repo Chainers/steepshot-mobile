2abb097a8f2eba86884444906c099a91d75e0c7c
Nikita Rakitin <nnn93@mail.ru>
2018-02-12 09:55:27 +0300

    package update + new logo

--- Sources/Steepshot/Steepshot.iOS/LinkerPleaseInclude.cs
+++ Sources/Steepshot/Steepshot.iOS/LinkerPleaseInclude.cs
@@ -3,6 +3,6 @@ public class LinkerPleaseInclude
 {
     public void Include()
     {
-        var x = new System.ComponentModel.ReferenceConverter(typeof(void));
+        //var x = new System.ComponentModel.ReferenceConverter(typeof(void));
     }
 }
