ab7e84ab371f5cdacbfb19146623e9e6d98b4405
KorzunAV <KorzunAV@gmail.com>
2018-03-22 19:15:34 +0300

    https://chainers.atlassian.net/browse/STEE-535

--- Sources/Steepshot/Steepshot.Android/Utils/MessageHelper.cs
+++ Sources/Steepshot/Steepshot.Android/Utils/MessageHelper.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using Android.App;
 using Android.Content;
+using Android.Text.Method;
+using Android.Text.Util;
+using Android.Views;
 using Android.Widget;
 using Steepshot.Core.Errors;
 using Steepshot.Core.Localization;
@@ -15,39 +18,21 @@ namespace Steepshot.Utils
             if (error == null || error is CanceledError)
                 return;
 
-            var message = error.Message;
+            var message = GetMsg(error);
             if (string.IsNullOrWhiteSpace(message))
                 return;
 
-            var alert = new AlertDialog.Builder(context);
-            var lm = AppSettings.LocalizationManager;
-            if (!lm.ContainsKey(message))
-            {
-                if (error is BlockchainError blError)
-                {
-                    AppSettings.Reporter.SendMessage($"New message: {LocalizationManager.NormalizeKey(blError.Message)}{Environment.NewLine}Full Message:{blError.FullMessage}");
-                }
-                else
-                {
-                    AppSettings.Reporter.SendMessage($"New message: {LocalizationManager.NormalizeKey(message)}");
-                }
-                message = nameof(LocalizationKeys.UnexpectedError);
-            }
-
-            alert.SetMessage(lm.GetText(message));
-            alert.SetPositiveButton(lm.GetText(LocalizationKeys.Ok), (senderAlert, args) => { });
-            Dialog dialog = alert.Create();
-            dialog.Show();
+            var btnOk = AppSettings.LocalizationManager.GetText(LocalizationKeys.Ok);
+            CreateAndShowDialog(context, message, btnOk);
         }
 
         public static void ShowAlert(this Context context, LocalizationKeys key)
         {
             var lm = AppSettings.LocalizationManager;
-            var alert = new AlertDialog.Builder(context);
-            alert.SetMessage(lm.GetText(key));
-            alert.SetPositiveButton(lm.GetText(LocalizationKeys.Ok), (senderAlert, args) => { });
-            Dialog dialog = alert.Create();
-            dialog.Show();
+            var btnOk = lm.GetText(LocalizationKeys.Ok);
+            var msg = lm.GetText(key);
+
+            CreateAndShowDialog(context, msg, btnOk);
         }
 
         public static void ShowAlert(this Context context, LocalizationKeys keys, ToastLength length)
@@ -61,25 +46,12 @@ namespace Steepshot.Utils
             if (error == null || error is CanceledError)
                 return;
 
-            var message = error.Message;
+            var message = GetMsg(error);
+
             if (string.IsNullOrWhiteSpace(message))
                 return;
 
-            var lm = AppSettings.LocalizationManager;
-            if (!lm.ContainsKey(message))
-            {
-                if (error is BlockchainError blError)
-                {
-                    AppSettings.Reporter.SendMessage($"New message: {LocalizationManager.NormalizeKey(blError.Message)}{Environment.NewLine}Full Message:{blError.FullMessage}");
-                }
-                else
-                {
-                    AppSettings.Reporter.SendMessage($"New message: {LocalizationManager.NormalizeKey(message)}");
-                }
-                message = nameof(LocalizationKeys.UnexpectedError);
-            }
-
-            Toast.MakeText(context, lm.GetText(message), length).Show();
+            Toast.MakeText(context, message, length).Show();
         }
 
         public static void ShowInteractiveMessage(this Context context, ErrorBase error, EventHandler<DialogClickEventArgs> tryAgainAction, EventHandler<DialogClickEventArgs> forgetAction)
@@ -87,9 +59,51 @@ namespace Steepshot.Utils
             if (error == null || error is CanceledError)
                 return;
 
-            var message = error.Message;
+            var message = GetMsg(error);
             if (string.IsNullOrWhiteSpace(message))
                 return;
+
+            var lm = AppSettings.LocalizationManager;
+            var pBtn = lm.GetText(LocalizationKeys.TryAgain);
+            var nBtn = lm.GetText(LocalizationKeys.Forget);
+            CreateAndShowDialog(context, message, pBtn, tryAgainAction, nBtn, forgetAction);
+        }
+
+        private static void CreateAndShowDialog(Context context, string msg,
+            string positiveButtonText, EventHandler<DialogClickEventArgs> positiveButtonAction = null,
+            string negativeButtonText = null, EventHandler<DialogClickEventArgs> negativeButtonAction = null)
+        {
+            var alert = new AlertDialog.Builder(context);
+            if (msg.Contains("https:"))
+            {
+                var inflater = (LayoutInflater)context.GetSystemService(Context.LayoutInflaterService);
+                var alertView = inflater.Inflate(Resource.Layout.lyt_alert, null);
+                var textView = (TextView)alertView.FindViewById(Resource.Id.alert);
+                textView.SetText(msg, TextView.BufferType.Normal);
+                alert.SetView(alertView);
+            }
+            else
+            {
+                alert.SetMessage(msg);
+            }
+
+            if (positiveButtonAction == null)
+                positiveButtonAction = (senderAlert, args) => { };
+
+            alert.SetPositiveButton(positiveButtonText, positiveButtonAction);
+
+            if (!string.IsNullOrEmpty(negativeButtonText))
+                alert.SetNegativeButton(negativeButtonText, negativeButtonAction);
+
+            Dialog dialog = alert.Create();
+            dialog.Show();
+        }
+
+        private static string GetMsg(ErrorBase error)
+        {
+            var message = error.Message;
+            if (string.IsNullOrWhiteSpace(message))
+                return message;
 
             var lm = AppSettings.LocalizationManager;
             if (!lm.ContainsKey(message))
@@ -105,12 +119,7 @@ namespace Steepshot.Utils
                 message = nameof(LocalizationKeys.UnexpectedError);
             }
 
-            var alert = new AlertDialog.Builder(context);
-            alert.SetMessage(lm.GetText(message));
-            alert.SetNegativeButton(lm.GetText(LocalizationKeys.Forget), forgetAction);
-            alert.SetPositiveButton(lm.GetText(LocalizationKeys.TryAgain), tryAgainAction);
-            Dialog dialog = alert.Create();
-            dialog.Show();
+            return lm.GetText(message);
         }
     }
 }
