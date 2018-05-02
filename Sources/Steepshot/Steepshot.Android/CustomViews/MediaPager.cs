039492df39e4a02d890573638628e371a18413e5
Stanislav <ugnivs@live.com>
2018-05-02 12:32:45 +0300

    UI fixes

---
+++ Sources/Steepshot/Steepshot.Android/CustomViews/MediaPager.cs
@@ -0,0 +1,25 @@
+﻿using Android.Content;
+using Android.Graphics;
+using Android.Support.V4.View;
+using Android.Util;
+
+namespace Steepshot.CustomViews
+{
+    public class MediaPager : ViewPager
+    {
+        public int Radius { get; set; }
+        public MediaPager(Context context, IAttributeSet attrs) : base(context, attrs)
+        {
+        }
+
+        protected override void OnDraw(Canvas canvas)
+        {
+            base.OnDraw(canvas);
+            canvas.DrawFilter = new PaintFlagsDrawFilter(PaintFlags.AntiAlias, PaintFlags.AntiAlias);
+
+            var clipPath = new Path();
+            clipPath.AddRoundRect(ScrollX, 0, ScrollX + Width, Height, Radius, Radius, Path.Direction.Cw);
+            canvas.ClipPath(clipPath);
+        }
+    }
+}
---
+++ Sources/Steepshot/Steepshot.Android/Utils/ViewUtils.cs
@@ -0,0 +1,14 @@
+﻿using Android.Views;
+using Java.Lang;
+using Java.Lang.Reflect;
+
+namespace Steepshot.Utils
+{
+    public static class ViewUtils
+    {
+        private static readonly Method _toLocalTouchEvent = Class.FromType(typeof(View))
+            .GetDeclaredMethod("toLocalMotionEvent", Class.FromType(typeof(MotionEvent)));
+
+        public static bool ToLocalTouchEvent(this View view, MotionEvent ev) => (bool)_toLocalTouchEvent?.Invoke(view, ev);
+    }
+}
