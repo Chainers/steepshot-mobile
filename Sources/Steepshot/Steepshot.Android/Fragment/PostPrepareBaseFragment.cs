e44c7bf94323febc1f82e17a269c1719a82ac664
KorzunAV <KorzunAV@gmail.com>
2018-05-04 13:17:43 +0300

    filter fix

--- Sources/Steepshot/Steepshot.Android/Fragment/PostPrepareBaseFragment.cs
+++ Sources/Steepshot/Steepshot.Android/Fragment/PostPrepareBaseFragment.cs
@@ -99,7 +99,7 @@ namespace Steepshot.Fragment
             base.OnViewCreated(view, savedInstanceState);
 
             _tag.Hint = AppSettings.LocalizationManager.GetText(LocalizationKeys.Hashtag);
-            //_tag.SetFilters(new IInputFilter[] { new TextInputFilter(@"[a-zа-я0-9- ]+") });
+            _tag.SetFilters(new IInputFilter[] { new TextInputFilter(TextInputFilter.TagFilter) });
             _tagLabel.Text = AppSettings.LocalizationManager.GetText(LocalizationKeys.Hashtag);
             _title.Hint = AppSettings.LocalizationManager.GetText(LocalizationKeys.EnterPostTitle);
             _description.Hint = AppSettings.LocalizationManager.GetText(LocalizationKeys.EnterPostDescription);
--- Sources/Steepshot/Steepshot.Android/Utils/TextInputFilter.cs
+++ Sources/Steepshot/Steepshot.Android/Utils/TextInputFilter.cs
@@ -1,4 +1,4 @@
-﻿using System.Text.RegularExpressions;
+﻿using System.Collections.Generic;
 using Android.Text;
 using Java.Lang;
 
@@ -6,29 +6,58 @@ namespace Steepshot.Utils
 {
     public class TextInputFilter : Object, IInputFilter
     {
-        private readonly Regex _chars;
+        public const string RuLang = "йцукенгшщзхъфывапролджэячсмитьбюЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТТЬБЮ";
+        public const string EnLang = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM";
+        public const string Digits = "0123456789";
+        public const string TagFilter = RuLang + EnLang + Digits + "-" + " ";
+
+        private readonly HashSet<char> _chars;
 
         public TextInputFilter(string pattern)
         {
-            _chars = new Regex(pattern, RegexOptions.IgnoreCase);
+            _chars = new HashSet<char>(pattern);
         }
 
         public ICharSequence FilterFormatted(ICharSequence source, int start, int end, ISpanned dest, int dstart, int dend)
         {
-            if (source != null)
+            if (source != null && source.Length() > 0)
             {
-                var txt = source.ToString();
-                var matches = _chars.Matches(txt);
-                if (matches.Count > 0)
-                {
-                    var newtext = string.Empty;
-                    for (var i = 0; i < matches.Count; i++)
-                        newtext += matches[i].Value;
+                var includesInvalidCharacter = false;
 
-                    return new String(newtext);
+                var destLength = dend - dstart + 1;
+                var adjustStart = source.Length() - destLength;
+
+                var sb = new StringBuilder(end - start);
+                for (var i = start; i < end; i++)
+                {
+                    var c = source.CharAt(i);
+                    if (IsCharAllowed(c))
+                    {
+                        if (i >= adjustStart)
+                            sb.Append(source, i, i + 1);
+                    }
+                    else
+                        includesInvalidCharacter = true;
                 }
+
+                if (!includesInvalidCharacter)
+                    return null;
+
+                if (source is ISpanned spanned)
+                {
+                    var sp = new SpannableString(sb);
+                    TextUtils.CopySpansFrom(spanned, start, sb.Length(), null, sp, 0);
+                    return sp;
+                }
+
+                return sb;
             }
-            return new String();
+            return null;
+        }
+
+        private bool IsCharAllowed(char c)
+        {
+            return _chars.Contains(c);
         }
     }
 }
