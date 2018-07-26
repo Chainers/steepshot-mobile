c4c48d867075efc36daba0909aaa20d195a9f8f3
Maxim <imulir@gmail.com>
2018-07-26 15:57:46 +0300

    added view

---
+++ Sources/Steepshot/Steepshot.Android/Fragment/PlagiarismCheckFragment.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using CheeseBind;
+using Steepshot.Base;
+using Steepshot.Core.Presenters;
+
+namespace Steepshot.Activity
+{
+    public class PlagiarismCheckFragment : BaseFragmentWithPresenter<PostDescriptionPresenter>
+    {
+        public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+        {
+            if (!IsInitialized)
+            {
+                InflatedView = inflater.Inflate(Resource.Layout.lyt_plagiarism, null);
+                Cheeseknife.Bind(this, InflatedView);
+            }
+
+            return InflatedView;
+        }
+
+        public override void OnViewCreated(View view, Bundle savedInstanceState)
+        { 
+            if (IsInitialized)
+                return;
+
+            base.OnViewCreated(view, savedInstanceState);
+        }
+    }
+}
