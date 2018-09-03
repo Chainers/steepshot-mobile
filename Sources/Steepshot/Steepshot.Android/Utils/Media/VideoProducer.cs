48f4b8a3e444efed0d2aacbb6c7fa8906e83d0f5
KorzunAV <KorzunAV@gmail.com>
2018-09-03 14:02:10 +0300

    cros protocol fix

--- Sources/Steepshot/Steepshot.Android/Utils/Media/VideoProducer.cs
+++ Sources/Steepshot/Steepshot.Android/Utils/Media/VideoProducer.cs
@@ -2,12 +2,11 @@
 using Android.Media;
 using Android.Views;
 using Com.Google.Android.Exoplayer2;
-using Com.Google.Android.Exoplayer2.Extractor;
-using Com.Google.Android.Exoplayer2.Source;
 using Com.Google.Android.Exoplayer2.Source.Hls;
 using Com.Google.Android.Exoplayer2.Trackselection;
 using Com.Google.Android.Exoplayer2.Upstream;
 using Com.Google.Android.Exoplayer2.Util;
+using Steepshot.Core;
 using Steepshot.Core.Models.Common;
 
 namespace Steepshot.Utils.Media
@@ -18,7 +17,10 @@ namespace Steepshot.Utils.Media
         private readonly IMediaPerformer _mediaPerformer;
         private SimpleExoPlayer _player;
         private MediaModel _media;
-        private Context _context;
+        private readonly Context _context;
+        private static readonly DefaultBandwidthMeter BandwidthMeter = new DefaultBandwidthMeter();
+        private HlsMediaSource.Factory _extractorMediaSource;
+
 
         public VideoProducer(Context context, IMediaPerformer mediaPerformer)
         {
@@ -28,21 +30,13 @@ namespace Steepshot.Utils.Media
 
         public void Init(MediaModel media)
         {
-            _media = media;
-
-            var defaultBandwidthMeter = new DefaultBandwidthMeter();
-            var adaptiveTrackSelectionFactory = new AdaptiveTrackSelection.Factory(defaultBandwidthMeter);
+            var adaptiveTrackSelectionFactory = new AdaptiveTrackSelection.Factory(BandwidthMeter);
             var defaultTrackSelector = new DefaultTrackSelector(adaptiveTrackSelectionFactory);
             var defaultRenderersFactory = new DefaultRenderersFactory(_context);
 
             _player = ExoPlayerFactory.NewSimpleInstance(defaultRenderersFactory, defaultTrackSelector);
-            //_player?.SetOnBufferingUpdateListener(this);
-            //_player?.SetOnCompletionListener(this);
-            //_player?.SetOnPreparedListener(this);
-            //_player?.SetAudioStreamType(Stream.Music);
-            //_player?.SetDataSource(_videoUrl);
-
-
+            
+            _media = media;
         }
 
         public void OnBufferingUpdate(MediaPlayer mp, int percent)
@@ -55,37 +49,31 @@ namespace Steepshot.Utils.Media
 
         public void OnPrepared(MediaPlayer mp)
         {
-            //    _player?.Start();
-            //    _player?.Pause();
+            _player.PlayWhenReady = false;
         }
 
         public void Pause()
         {
-            //    _player?.Stop(false);
+            _player.PlayWhenReady = false;
         }
 
         public void Play()
         {
-            //    _player?.Start();
+            _player.SeekTo(0);
+            _player.PlayWhenReady = true;
         }
 
         public void Prepare()
         {
-            var mediaUri = Android.Net.Uri.Parse(_media.Url);//https://s3-us-west-2.amazonaws.com/hls-playground/hls.m3u8
-            var userAgent = Util.GetUserAgent(_context, "ExoPlayerDemo");
-            var defaultHttpDataSourceFactory = new DefaultHttpDataSourceFactory(userAgent);
+            var userAgent = Util.GetUserAgent(_context, Constants.Steepshot);
+            var defaultHttpDataSourceFactory = new DefaultHttpDataSourceFactory(userAgent, BandwidthMeter, 30000, 30000, true);
             var defaultDataSourceFactory = new DefaultDataSourceFactory(_context, null, defaultHttpDataSourceFactory);
-            var extractorMediaSource = new HlsMediaSource.Factory(defaultDataSourceFactory);
+
+            _extractorMediaSource = new HlsMediaSource.Factory(defaultDataSourceFactory);
             _player.SetVideoTextureView((TextureView)_mediaPerformer);
-            _player.PlayWhenReady = true;
-            _player.Prepare(extractorMediaSource.CreateMediaSource(mediaUri));
 
-
-            //var texture = (TextureView)_mediaPerformer;
-            //if (!texture.IsAvailable) return;
-            //var surface = new Surface(texture.SurfaceTexture);
-            //_player?.SetSurface(surface);
-            //_player?.PrepareAsync();
+            var mediaUri = Android.Net.Uri.Parse(_media.Url);
+            _player.Prepare(_extractorMediaSource.CreateMediaSource(mediaUri));
         }
 
         public void Release()
